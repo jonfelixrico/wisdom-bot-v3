bd76083bba1d1b771857a43081f9e4e60cc4b06a
Felix <jonfelixrico@gmail.com>
2021-05-31 21:09:25 +0800

    Provide the repositories for all current entities in RepositoriesModule

--- src/repositories/repositories.module.ts
+++ src/repositories/repositories.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common'
 import { ReceiveRepoImplService } from './receive-repo-impl/receive-repo-impl.service'
 import { QuoteRepoImplService } from './quote-repo-impl/quote-repo-impl.service'
 import { ConcurRepoImplService } from './concur-repo-impl/concur-repo-impl.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Approval } from 'src/entities/approval.entity'
+import { Quote } from 'src/entities/quote.entity'
+import { Concur } from 'src/entities/concur.entity'
+import { Receive } from 'src/entities/receive.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Approval, Quote, Concur, Receive])],
   providers: [
     ReceiveRepoImplService,
     QuoteRepoImplService,
