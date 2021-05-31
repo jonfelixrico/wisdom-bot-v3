4a2f24306bbe15ca4aa56860c5bd99455ce7a8e8
Felix <jonfelixrico@gmail.com>
2021-05-31 19:38:43 +0800

    Use the config service in the discord provider

--- src/discord/discord.provider.ts
+++ src/discord/discord.provider.ts
@@ -1,18 +1,20 @@
 import { Provider } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Client } from 'discord.js'
 
 export const discordProvider: Provider = {
   provide: Client,
-  useFactory: () =>
+  useFactory: (cfg: ConfigService) =>
     new Promise(async (resolve, reject) => {
       try {
         const client = new Client()
         client.once('ready', () => {
           resolve(client)
         })
-        await client.login(process.env.DISCORD_TOKEN)
+        await client.login(cfg.get('DISCORD_TOKEN'))
       } catch (e) {
         reject(e)
       }
     }),
+  inject: [ConfigService],
 }
