3ee96574f141e6d1c9fb1326513a6a487fc34434
Felix <jonfelixrico@gmail.com>
2021-05-31 20:12:02 +0800

    Integrate TypeOrm module into the app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common'
-import { discordProvider } from './discord/discord.provider'
 import { DiscordModule } from './discord/discord.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  providers: [discordProvider],
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.dev.env',
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (cfg: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: cfg.get('DB_HOST'),
+          port: cfg.get('DB_PORT'),
+          database: cfg.get('DB_DATABSE'),
+          username: cfg.get('DB_USERNAME'),
+          password: cfg.get('DB_PASSWORD'),
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: !!cfg.get('DB_SYNC'),
+        }
+      },
+      inject: [ConfigService],
     }),
     DiscordModule,
   ],
