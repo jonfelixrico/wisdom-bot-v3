a3974cf2f63217706901e0986c424c3af45b8e10
Felix <jonfelixrico@gmail.com>
2021-05-31 22:28:52 +0800

    Add implementation to ReceiveRepoImplService#createReceive

--- src/repositories/receive-repo-impl/receive-repo-impl.service.ts
+++ src/repositories/receive-repo-impl/receive-repo-impl.service.ts
@@ -1,4 +1,59 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import {
+  INewReceive,
+  IReceive,
+  ReceiveRepository,
+} from 'src/classes/receive-repository.abstract'
+import { Receive } from 'src/entities/receive.entity'
+import { Repository } from 'typeorm'
+import { Quote } from 'src/entities/quote.entity'
+
+async function convertReceiveEntToObject({
+  quote: pQuote,
+  channelId,
+  guildId,
+  userId,
+  receiveDt,
+  id: receiveId,
+}: Receive): Promise<IReceive> {
+  const quote = await pQuote
+
+  return {
+    channelId,
+    guildId,
+    userId,
+    receiveDt,
+    quoteId: quote.id,
+    receiveId,
+  }
+}
 
 @Injectable()
-export class ReceiveRepoImplService {}
+export class ReceiveRepoImplService extends ReceiveRepository {
+  constructor(
+    @InjectRepository(Receive) private recvRepo: Repository<Receive>,
+    @InjectRepository(Quote) private quoteRepo: Repository<Quote>,
+  ) {
+    super()
+  }
+
+  async createRecieve({
+    quoteId,
+    channelId,
+    guildId,
+    userId,
+  }: INewReceive): Promise<[IReceive, number]> {
+    const quote = await this.quoteRepo.findOne({ id: quoteId })
+    const receives = await quote.receives
+
+    const newReceive = await this.recvRepo.create({
+      quote: Promise.resolve(quote),
+      channelId,
+      guildId,
+      userId,
+    })
+
+    return [await convertReceiveEntToObject(newReceive), receives.length + 1]
+  }
+}
