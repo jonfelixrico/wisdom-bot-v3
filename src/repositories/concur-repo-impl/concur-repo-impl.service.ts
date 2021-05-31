c72cd9e0e0d523bef6471a50bae85d026c682b4c
Felix <jonfelixrico@gmail.com>
2021-05-31 22:42:16 +0800

    Push implementation of ConcurRepoImplService#createConcur

--- src/repositories/concur-repo-impl/concur-repo-impl.service.ts
+++ src/repositories/concur-repo-impl/concur-repo-impl.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import {
+  ConcurRepository,
+  IConcur,
+  INewConcur,
+} from 'src/classes/concur-repository.abstract'
+import { Concur } from 'src/entities/concur.entity'
+import { Receive } from 'src/entities/receive.entity'
+import { Repository } from 'typeorm'
 
 @Injectable()
-export class ConcurRepoImplService {}
+export class ConcurRepoImplService extends ConcurRepository {
+  constructor(
+    @InjectRepository(Concur)
+    private concurRepo: Repository<Concur>,
+  ) {
+    super()
+  }
+
+  private getConcurCount(receiveId: string) {
+    return this.concurRepo
+      .createQueryBuilder()
+      .where('receiveId = :receiveId', { receiveId })
+      .getCount()
+  }
+
+  async createConcur({
+    channelId,
+    guildId,
+    receiveId,
+    userId,
+  }: INewConcur): Promise<[IConcur, number]> {
+    const receive = new Receive()
+    receive.id = receiveId
+
+    const { id: concurId, concurDt } = await this.concurRepo.create({
+      receive: Promise.resolve(receive),
+      channelId,
+      guildId,
+      userId,
+      concurDt: new Date(),
+    })
+
+    return [
+      {
+        channelId,
+        guildId,
+        receiveId,
+        userId,
+        concurDt,
+        concurId,
+      },
+      await this.getConcurCount(receiveId),
+    ]
+  }
+}
