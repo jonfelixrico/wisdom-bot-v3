e76db08a6bea6baee953f952793b2c1dcb3d6371
Felix <jonfelixrico@gmail.com>
2021-06-01 19:45:34 +0800

    Define imported modules in the CommandoModule

--- src/commando/commando.module.ts
+++ src/commando/commando.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { SubmitCommandService } from './submit-command/submit-command.service'
 import { ReceiveCommandService } from './receive-command/receive-command.service'
+import { DiscordModule } from 'src/discord/discord.module'
+import { RepositoriesModule } from 'src/repositories/repositories.module'
 
 @Module({
+  imports: [DiscordModule, RepositoriesModule],
   providers: [SubmitCommandService, ReceiveCommandService],
 })
 export class CommandoModule {}
