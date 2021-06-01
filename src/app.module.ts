8244a9363a855ffad2a184883ee2948614a31e0f
Felix <jonfelixrico@gmail.com>
2021-06-01 19:41:59 +0800

    Remove the CommandModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 import { DiscordModule } from './discord/discord.module'
 import { ConfigModule } from '@nestjs/config'
 import { RepositoriesModule } from './repositories/repositories.module'
-import { CommandsModule } from './commands/controllers.module'
 import { TypeormModule } from './typeorm/typeorm.module'
 
 @Module({
@@ -14,7 +13,6 @@ import { TypeormModule } from './typeorm/typeorm.module'
     TypeormModule,
     DiscordModule,
     RepositoriesModule,
-    CommandsModule,
   ],
 })
 export class AppModule {}
--- src/commands/controllers.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common'
-import { DiscordModule } from 'src/discord/discord.module'
-
-@Module({
-  imports: [DiscordModule],
-})
-export class CommandsModule {}
