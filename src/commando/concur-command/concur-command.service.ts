31745243c1407c5dbdffc66095f2adf6370436a3
Felix <jonfelixrico@gmail.com>
2021-06-01 22:05:25 +0800

    Push placeholder code for the concur command functionality

--- src/commando/concur-command/concur-command.service.ts
+++ src/commando/concur-command/concur-command.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common'
+import { Message } from 'discord.js'
+import {
+  CommandInfo,
+  CommandoClient,
+  CommandoMessage,
+} from 'discord.js-commando'
+import { WrappedCommand } from '../wrapped-command.class'
+
+const COMMAND_INFO: CommandInfo = {
+  name: 'concur',
+  aliases: ['amen'],
+  description: 'Agree to a receive.',
+  memberName: 'concur',
+  group: 'commands',
+}
 
 @Injectable()
-export class ConcurCommandService {}
+export class ConcurCommandService extends WrappedCommand {
+  constructor(client: CommandoClient) {
+    super(client, COMMAND_INFO)
+  }
+
+  async run({
+    channel,
+    reference,
+  }: CommandoMessage): Promise<Message | Message[]> {
+    return await channel.send([channel.id, reference?.messageID].join('/'))
+  }
+}
