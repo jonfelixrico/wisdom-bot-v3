45e02c542b497cb3d2581e89a18b611188e5bd52
Felix <jonfelixrico@gmail.com>
2021-06-01 20:50:57 +0800

    Implement placeholder for ReceiveCommandService

--- src/commando/receive-command/receive-command.service.ts
+++ src/commando/receive-command/receive-command.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common'
+import { Message } from 'discord.js'
+import {
+  CommandoMessage,
+  ArgumentCollectorResult,
+  CommandoClient,
+  CommandInfo,
+} from 'discord.js-commando'
+import { UnknownRecord, WrappedCommand } from '../wrapped-command.class'
+
+const COMMAND_INFO: CommandInfo = {
+  name: 'receive',
+  group: 'commands',
+  memberName: 'receive',
+  description: 'Receive a random command.',
+  argsPromptLimit: 0,
+  args: [
+    {
+      key: 'user',
+      type: 'user',
+      prompt: '',
+      default: () => null,
+    },
+  ],
+}
 
 @Injectable()
-export class ReceiveCommandService {}
+export class ReceiveCommandService extends WrappedCommand {
+  run(
+    message: CommandoMessage,
+    args: string | UnknownRecord | string[],
+    fromPattern: boolean,
+    result?: ArgumentCollectorResult<UnknownRecord>,
+  ): Promise<Message | Message[]> {
+    throw new Error('Method not implemented.')
+  }
+  constructor(client: CommandoClient) {
+    super(client, COMMAND_INFO)
+  }
+}
