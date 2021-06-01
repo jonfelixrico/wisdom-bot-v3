6e0724a03a63675bf5cc5b6ec6cd08b15f5ee6a1
Felix <jonfelixrico@gmail.com>
2021-06-01 20:50:33 +0800

    Apply minor improvements to the WrappedCommand abstract class

--- src/commando/wrapped-command.class.ts
+++ src/commando/wrapped-command.class.ts
@@ -1,8 +1,24 @@
-import { Command, CommandInfo, CommandoClient } from 'discord.js-commando'
+import { Message } from 'discord.js'
+import {
+  ArgumentCollectorResult,
+  Command,
+  CommandInfo,
+  CommandoClient,
+  CommandoMessage,
+} from 'discord.js-commando'
+
+export type UnknownRecord = Record<string, unknown>
 
 export abstract class WrappedCommand extends Command {
   constructor(client: CommandoClient, info: CommandInfo) {
     super(client, info)
     client.registry.registerCommand(this)
   }
+
+  abstract run(
+    message: CommandoMessage,
+    args: string | UnknownRecord | string[],
+    fromPattern: boolean,
+    result?: ArgumentCollectorResult<UnknownRecord>,
+  ): Promise<Message | Message[]>
 }
