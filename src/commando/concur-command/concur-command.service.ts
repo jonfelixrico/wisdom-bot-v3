4c7c42c82a636abd65d8a41f0555f6ff385dff82
Felix <jonfelixrico@gmail.com>
2021-06-01 23:07:46 +0800

    Implement concur functionality

--- src/commando/concur-command/concur-command.service.ts
+++ src/commando/concur-command/concur-command.service.ts
@@ -5,6 +5,8 @@ import {
   CommandoClient,
   CommandoMessage,
 } from 'discord.js-commando'
+import { ConcurRepository } from 'src/classes/concur-repository.abstract'
+import { ReceiveRepository } from 'src/classes/receive-repository.abstract'
 import { WrappedCommand } from '../wrapped-command.class'
 
 const COMMAND_INFO: CommandInfo = {
@@ -17,14 +19,40 @@ const COMMAND_INFO: CommandInfo = {
 
 @Injectable()
 export class ConcurCommandService extends WrappedCommand {
-  constructor(client: CommandoClient) {
+  constructor(
+    client: CommandoClient,
+    private recvRepo: ReceiveRepository,
+    private concurRepo: ConcurRepository,
+  ) {
     super(client, COMMAND_INFO)
   }
 
   async run({
     channel,
     reference,
+    author,
+    guild,
   }: CommandoMessage): Promise<Message | Message[]> {
-    return await channel.send([channel.id, reference?.messageID].join('/'))
+    if (!reference) {
+      return await channel.send('Invalid invocation.')
+    }
+
+    const receive = await this.recvRepo.findReceiveByMessageId(
+      reference.messageID,
+    )
+
+    if (!receive) {
+      return await channel.send('Invalid invocation.')
+    }
+
+    const [concur, concurCount] = await this.concurRepo.createConcur({
+      channelId: channel.id,
+      // TODO remove guild and channel information
+      guildId: guild.id,
+      userId: author.id,
+      receiveId: receive.receiveId,
+    })
+
+    return await channel.send([JSON.stringify(concur), concurCount].join('/'))
   }
 }
