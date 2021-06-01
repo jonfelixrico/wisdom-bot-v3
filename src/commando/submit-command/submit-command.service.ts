261ffe326ca3a214c3bc3aa0b64afdcf9e73930e
Felix <jonfelixrico@gmail.com>
2021-06-01 20:40:13 +0800

    Implement cleaner implementation of SubmitCommandService

--- src/commando/submit-command/submit-command.service.ts
+++ src/commando/submit-command/submit-command.service.ts
@@ -1,36 +1,48 @@
 import { Injectable } from '@nestjs/common'
 import { Message } from 'discord.js'
-import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando'
+import {
+  CommandInfo,
+  CommandoClient,
+  CommandoMessage,
+} from 'discord.js-commando'
+import { WrappedCommand } from '../wrapped-command.class'
+
+const COMMAND_INFO: CommandInfo = {
+  name: 'submit',
+  aliases: ['add'],
+  group: 'commands',
+  memberName: 'submit',
+  description:
+    'Submit a new quote. This quote has to go through an approval process.',
+  args: [
+    {
+      key: 'author',
+      type: 'user',
+      prompt: '',
+    },
+    {
+      key: 'quote',
+      type: 'string',
+      prompt: '',
+    },
+    {
+      key: 'year',
+      type: 'number',
+      max: 3000,
+      min: 1970,
+      prompt: '',
+    },
+  ],
+  argsPromptLimit: 0,
+}
 
 @Injectable()
-export class SubmitCommandService extends Command {
+export class SubmitCommandService extends WrappedCommand {
   constructor(client: CommandoClient) {
-    super(client, {
-      name: 'submit',
-      aliases: ['add'],
-      group: 'commands',
-      memberName: 'submit',
-      description:
-        'Submit a new quote. This quote has to go through an approval process.',
-      args: [
-        {
-          key: 'quote',
-          type: 'string',
-          prompt: 'Test prompt',
-        },
-        {
-          key: 'author',
-          type: 'user',
-          prompt: 'Test prompt 2',
-        },
-      ],
-      argsPromptLimit: 0,
-    })
-
-    client.registry.registerCommand(this)
+    super(client, COMMAND_INFO)
   }
 
   public async run(message: CommandoMessage): Promise<Message | Message[]> {
-    return await message.channel.send('hi!')
+    return await message.channel.send('Received submission.')
   }
 }
