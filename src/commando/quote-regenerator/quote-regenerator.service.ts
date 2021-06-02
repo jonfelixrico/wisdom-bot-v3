d1e82e5e4bcf89a188377deb57036a3773c58ffe
Felix <jonfelixrico@gmail.com>
2021-06-02 23:24:45 +0800

    Apply additional logic to QuoteRegeneratorService

--- src/commando/quote-regenerator/quote-regenerator.service.ts
+++ src/commando/quote-regenerator/quote-regenerator.service.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@nestjs/common'
 import { QuoteRepository } from 'src/classes/quote-repository.abstract'
 import { GuildRepoService } from 'src/discord/guild-repo/guild-repo.service'
 import { DeleteListenerService } from '../delete-listener/delete-listener.service'
+import { ReactionListenerService } from '../reaction-listener/reaction-listener.service'
 
 @Injectable()
 export class QuoteRegeneratorService {
   constructor(
     private quoteRepo: QuoteRepository,
     private guildRepo: GuildRepoService,
-    private delListener: DeleteListenerService,
-  ) {}
+    private deleteListener: DeleteListenerService,
+    private reactionListener: ReactionListenerService,
+  ) {
+    this.setUp()
+  }
 
   setUp() {
-    this.delListener.delete$.subscribe((messageId) => {
+    this.deleteListener.emitter.subscribe((messageId) => {
       this.regenerateMessage(messageId)
     })
   }
@@ -29,7 +33,12 @@ export class QuoteRegeneratorService {
     const channel = await this.guildRepo.getTextChannel(guildId, channelId)
 
     const regenerated = await channel.send(['regenerated', quoteId].join('/'))
-    this.delListener.watch(messageId)
+    const expireDt = new Date()
+    expireDt.setMinutes(expireDt.getMinutes() + 2)
+    this.reactionListener.watch(regenerated.id, '🤔', 1, expireDt)
+    this.deleteListener.watch(regenerated.id)
     await this.quoteRepo.setMessageId(quoteId, regenerated.id)
+
+    await regenerated.react('🤔')
   }
 }
