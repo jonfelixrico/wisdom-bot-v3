f1a00643e42009340687a22dd1ace68a82e49b08
Felix <jonfelixrico@gmail.com>
2021-06-02 22:19:27 +0800

    Implement DeleteListenerService's functionality

--- src/commando/delete-listener/delete-listener.service.ts
+++ src/commando/delete-listener/delete-listener.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common'
+import { Client, Message } from 'discord.js'
+import { Observable, Subject } from 'rxjs'
+import { filter, map } from 'rxjs/operators'
 
 @Injectable()
-export class DeleteListenerService {}
+export class DeleteListenerService {
+  private deleteSubj = new Subject<Message>()
+  private watched = new Set<string>()
+
+  constructor(private client: Client) {
+    this.setUp()
+  }
+
+  private setUp() {
+    const { client, deleteSubj } = this
+
+    const emitMessageFn = (message: Message) => {
+      deleteSubj.next(message)
+    }
+
+    client.on('messageDelete', emitMessageFn)
+    client.on('messageDeleteBulk', (collection) => {
+      collection.forEach(emitMessageFn)
+    })
+  }
+
+  watch(messageId: string) {
+    this.watched.add(messageId)
+  }
+
+  unwatch(messageId: string) {
+    this.watched.delete(messageId)
+  }
+
+  get delete$(): Observable<string> {
+    return this.deleteSubj.pipe(
+      filter(({ author }) => author.id === this.client.user.id),
+      filter(({ id }) => this.watched.has(id)),
+      map(({ id }) => id),
+    )
+  }
+}
