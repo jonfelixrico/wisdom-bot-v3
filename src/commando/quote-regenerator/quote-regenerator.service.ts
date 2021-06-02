0150a27714d2f004826a1ebb9f378ca5a5a2427b
Felix <jonfelixrico@gmail.com>
2021-06-02 22:20:09 +0800

    Implement functionality of QuoteRegeneratorService

--- src/commando/quote-regenerator/quote-regenerator.service.ts
+++ src/commando/quote-regenerator/quote-regenerator.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common'
+import { QuoteRepository } from 'src/classes/quote-repository.abstract'
+import { GuildRepoService } from 'src/discord/guild-repo/guild-repo.service'
+import { DeleteListenerService } from '../delete-listener/delete-listener.service'
 
 @Injectable()
-export class QuoteRegeneratorService {}
+export class QuoteRegeneratorService {
+  constructor(
+    private quoteRepo: QuoteRepository,
+    private guildRepo: GuildRepoService,
+    private delListener: DeleteListenerService,
+  ) {}
+
+  setUp() {
+    this.delListener.delete$.subscribe((messageId) => {
+      this.regenerateMessage(messageId)
+    })
+  }
+
+  async regenerateMessage(messageId: string) {
+    const quote = await this.quoteRepo.getPendingQuoteByMessageId(messageId)
+    if (!quote) {
+      // TODO send warning/error
+      return
+    }
+
+    const { channelId, guildId, quoteId } = quote
+
+    const channel = await this.guildRepo.getTextChannel(guildId, channelId)
+
+    const regenerated = await channel.send(['regenerated', quoteId].join('/'))
+    this.delListener.watch(messageId)
+    await this.quoteRepo.setMessageId(quoteId, regenerated.id)
+  }
+}
