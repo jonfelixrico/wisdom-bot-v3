e1e7d62191f5d74f94a8a34c422ade313bee2b17
Felix <jonfelixrico@gmail.com>
2021-06-05 00:01:50 +0800

    Change default command prefix

--- src/commando/setup/setup.service.ts
+++ src/commando/setup/setup.service.ts
@@ -4,7 +4,7 @@ import { CommandoClient } from 'discord.js-commando'
 @Injectable()
 export class SetupService {
   constructor(client: CommandoClient) {
-    client.commandPrefix = '!wisdom'
+    client.commandPrefix = '~wisdom'
     client.registry.registerDefaultTypes()
     client.registry.registerDefaultGroups()
     client.registry.registerDefaultCommands()
