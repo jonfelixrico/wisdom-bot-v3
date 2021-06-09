61bc9a02089814acb5277983f69de2559114b2e8
Felix <jonfelixrico@gmail.com>
2021-06-09 23:44:55 +0800

    Implement logic for the SubmitQuoteHandler

--- src/command-handlers/submit-quote/submit-quote.cmd-handler.ts
+++ src/command-handlers/submit-quote/submit-quote.cmd-handler.ts
@@ -1,5 +1,13 @@
-import { CommandHandler } from '@nestjs/cqrs'
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs'
 import { SubmitQuote } from 'src/domain/pending-quote/submit-quote.command'
+import { PendingQuote } from 'src/domain/pending-quote/pending-quote.entity'
 
 @CommandHandler(SubmitQuote)
-export class SubmitQuoteHandler {}
+export class SubmitQuoteHandler implements ICommandHandler<SubmitQuote> {
+  constructor(private publisher: EventPublisher) {}
+  async execute({ submittedQuote }: SubmitQuote): Promise<any> {
+    const entity = PendingQuote.submit(submittedQuote)
+    const pendingQuote = this.publisher.mergeObjectContext(entity)
+    pendingQuote.commit()
+  }
+}
