ff8075d10f4ffd7a706f589f53ad1d6e76ffa267
Felix <jonfelixrico@gmail.com>
2021-06-10 23:08:54 +0800

    Implement handlers for receive quote and interact receive commands

--- src/command-handlers/quote/receive-quote-command-handler/receive-quote-command-handler.service.ts
+++ src/command-handlers/quote/receive-quote-command-handler/receive-quote-command-handler.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common'
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs'
+import { ReceiveQuote } from 'src/domain/quote/receive-quote.command'
+import { QuoteRepository } from 'src/domain/quote/quote.repository'
 
-@Injectable()
-export class ReceiveQuoteCommandHandlerService {}
+@CommandHandler(ReceiveQuote)
+export class ReceiveQuoteCommandHandlerService
+  implements ICommandHandler<ReceiveQuote>
+{
+  constructor(private pub: EventPublisher, private repo: QuoteRepository) {}
+
+  async execute({ payload }: ReceiveQuote): Promise<any> {
+    const fromRepo = await this.repo.findById(payload.quoteId)
+
+    if (!fromRepo) {
+      // TODO create custom error
+      throw new Error('Quote not found.')
+    }
+
+    const quote = this.pub.mergeObjectContext(fromRepo)
+    quote.receive(payload)
+    quote.commit()
+  }
+}
--- src/command-handlers/receive/interact-receive/interact-receive.service.ts
+++ src/command-handlers/receive/interact-receive/interact-receive.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common'
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs'
+import { InteractReceive } from 'src/domain/receive/interact-receive.command'
+import { ReceiveRepository } from 'src/domain/receive/receive.repository'
 
-@Injectable()
-export class InteractReceiveService {}
+@CommandHandler(InteractReceive)
+export class InteractReceiveService
+  implements ICommandHandler<InteractReceive>
+{
+  constructor(private pub: EventPublisher, private repo: ReceiveRepository) {}
+
+  async execute({ payload }: InteractReceive): Promise<any> {
+    const fromRepo = await this.repo.findById(payload.receiveId)
+
+    if (!fromRepo) {
+      // TODO throw custom error
+      throw new Error('Receive not found.')
+    }
+
+    const receive = this.pub.mergeObjectContext(fromRepo)
+    receive.interact(payload)
+    receive.commit()
+  }
+}
