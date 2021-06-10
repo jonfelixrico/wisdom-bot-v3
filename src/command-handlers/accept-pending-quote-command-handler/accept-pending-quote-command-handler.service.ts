65e146397f214484f788256febf2303b7a22efee
Felix <jonfelixrico@gmail.com>
2021-06-10 22:44:57 +0800

    Push implementations for the command handlers

--- src/command-handlers/accept-pending-quote-command-handler/accept-pending-quote-command-handler.service.ts
+++ src/command-handlers/accept-pending-quote-command-handler/accept-pending-quote-command-handler.service.ts
@@ -1,11 +1,27 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs'
 import { AcceptPendingQuote } from 'src/domain/pending-quote/accept-pending-quote.command'
+import { PendingQuoteRepository } from 'src/domain/pending-quote/pending-quote.repository'
 
 @CommandHandler(AcceptPendingQuote)
 export class AcceptPendingQuoteCommandHandlerService
   implements ICommandHandler<AcceptPendingQuote>
 {
-  execute(command: AcceptPendingQuote): Promise<any> {
-    throw new Error('Method not implemented.')
+  constructor(
+    private publisher: EventPublisher,
+    private repo: PendingQuoteRepository,
+  ) {}
+
+  async execute({ payload: quoteId }: AcceptPendingQuote): Promise<any> {
+    const fromRepo = await this.repo.findById(quoteId)
+
+    if (!fromRepo) {
+      // TODO throw handler-specific error message here
+      throw new Error('Quote not found.')
+    }
+
+    const quote = this.publisher.mergeObjectContext(fromRepo)
+
+    quote.accept()
+    quote.commit()
   }
 }
--- src/command-handlers/cancel-pending-quote-command-handler/cancel-pending-quote-command-handler.service.ts
+++ src/command-handlers/cancel-pending-quote-command-handler/cancel-pending-quote-command-handler.service.ts
@@ -1,11 +1,27 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs'
 import { CancelPendingQuote } from 'src/domain/pending-quote/cancel-pending-quote.command'
+import { PendingQuoteRepository } from 'src/domain/pending-quote/pending-quote.repository'
 
 @CommandHandler(CancelPendingQuote)
 export class CancelPendingQuoteCommandHandlerService
   implements ICommandHandler<CancelPendingQuote>
 {
-  execute(command: CancelPendingQuote): Promise<any> {
-    throw new Error('Method not implemented.')
+  constructor(
+    private publisher: EventPublisher,
+    private repo: PendingQuoteRepository,
+  ) {}
+
+  async execute({ payload: quoteId }: CancelPendingQuote): Promise<any> {
+    const fromRepo = await this.repo.findById(quoteId)
+
+    if (!fromRepo) {
+      // TODO throw handler-specific error message here
+      throw new Error('Quote not found.')
+    }
+
+    const quote = this.publisher.mergeObjectContext(fromRepo)
+
+    quote.cancel()
+    quote.commit()
   }
 }
