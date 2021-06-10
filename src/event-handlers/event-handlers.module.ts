c23bfb6686f990563ab0c6b5b4cdba06a9e032ec
Felix <jonfelixrico@gmail.com>
2021-06-11 00:23:32 +0800

    Implement the use of ESDB in GenericDomainEventHandler

--- src/event-handlers/event-handlers.module.ts
+++ src/event-handlers/event-handlers.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { EventSourcingModule } from 'src/event-sourcing/event-sourcing.module'
 import { GenericDomainEventHandlerService } from './generic-domain-event-handler/generic-domain-event-handler.service'
 
 @Module({
+  imports: [EventSourcingModule],
   providers: [GenericDomainEventHandlerService],
 })
 export class EventHandlersModule {}
--- src/event-handlers/generic-domain-event-handler/generic-domain-event-handler.service.ts
+++ src/event-handlers/generic-domain-event-handler/generic-domain-event-handler.service.ts
@@ -1,3 +1,4 @@
+import { EventStoreDBClient, jsonEvent } from '@eventstore/db-client'
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
 import { Event } from 'src/domain/event.abstract'
 
@@ -5,11 +6,21 @@ import { Event } from 'src/domain/event.abstract'
 export class GenericDomainEventHandlerService
   implements IEventHandler<Event<unknown>>
 {
-  handle(event: Event<unknown>) {
+  constructor(private esdbClient: EventStoreDBClient) {}
+
+  async handle(event: Event<unknown>) {
     if (!(event instanceof Event)) {
       return
     }
 
-    // TODO convert to event source here
+    const { eventName, aggregateId } = event
+    const payload = event.payload ?? {}
+
+    const esdbEvent = jsonEvent({
+      type: eventName,
+      data: payload,
+    })
+
+    await this.esdbClient.appendToStream(aggregateId, [esdbEvent])
   }
 }
