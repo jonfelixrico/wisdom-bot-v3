8276e5c62c2dec6d91a23cf2be5f88a1ff7cd05f
Felix <jonfelixrico@gmail.com>
2021-06-10 22:16:30 +0800

    Adjust PendingQuoteRepository to have the `create` method

--- src/domain/pending-quote/pending-quote.repository.ts
+++ src/domain/pending-quote/pending-quote.repository.ts
@@ -1,4 +1,7 @@
 import { Repository } from '../repository.abstract'
 import { PendingQuote } from './pending-quote.entity'
+import { IQuoteToSubmit } from './quote-to-submit.interface'
 
-export abstract class PendingQuoteRepository extends Repository<PendingQuote> {}
+export abstract class PendingQuoteRepository extends Repository<PendingQuote> {
+  abstract create(quote: IQuoteToSubmit): Promise<PendingQuote>
+}
