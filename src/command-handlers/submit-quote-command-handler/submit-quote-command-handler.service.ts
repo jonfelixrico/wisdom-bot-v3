bac19e594bd02828741b15d1dac0b03f373addb4
Felix <jonfelixrico@gmail.com>
2021-06-10 22:36:54 +0800

    Make SubmitQuoteCommandHandler use PendingQuoteRepository for creation

--- src/command-handlers/submit-quote-command-handler/submit-quote-command-handler.service.ts
+++ src/command-handlers/submit-quote-command-handler/submit-quote-command-handler.service.ts
@@ -1,15 +1,18 @@
 import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs'
-import { PendingQuote } from 'src/domain/pending-quote/pending-quote.entity'
+import { PendingQuoteRepository } from 'src/domain/pending-quote/pending-quote.repository'
 import { SubmitQuote } from 'src/domain/pending-quote/submit-quote.command'
 
 @CommandHandler(SubmitQuote)
 export class SubmitQuoteCommandHandlerService
   implements ICommandHandler<SubmitQuote>
 {
-  constructor(private publisher: EventPublisher) {}
+  constructor(
+    private publisher: EventPublisher,
+    private repo: PendingQuoteRepository,
+  ) {}
 
-  async execute({ payload: submittedQuote }: SubmitQuote): Promise<any> {
-    const entity = PendingQuote.submit(submittedQuote)
+  async execute({ payload }: SubmitQuote): Promise<any> {
+    const entity = await this.repo.create(payload)
     const pendingQuote = this.publisher.mergeObjectContext(entity)
     pendingQuote.commit()
   }
