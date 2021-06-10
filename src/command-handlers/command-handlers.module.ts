f0e640503ec72d5e2bc4f12743e459a2e5f346d2
Felix <jonfelixrico@gmail.com>
2021-06-10 22:37:12 +0800

    Generate boilerplate code for command handlers for AcceptPendingQuote and CancelPendingQuote

--- src/command-handlers/command-handlers.module.ts
+++ src/command-handlers/command-handlers.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common'
 import { SubmitQuoteCommandHandlerService } from './submit-quote-command-handler/submit-quote-command-handler.service'
+import { AcceptPendingQuoteCommandHandlerService } from './accept-pending-quote-command-handler/accept-pending-quote-command-handler.service'
+import { CancelPendingQuoteCommandHandlerService } from './cancel-pending-quote-command-handler/cancel-pending-quote-command-handler.service'
 
 @Module({
-  providers: [SubmitQuoteCommandHandlerService],
+  providers: [
+    SubmitQuoteCommandHandlerService,
+    AcceptPendingQuoteCommandHandlerService,
+    CancelPendingQuoteCommandHandlerService,
+  ],
 })
 export class CommandHandlersModule {}
