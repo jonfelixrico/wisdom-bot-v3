356e148b0ccb81360b2caff74a396b6092620cb1
Felix <jonfelixrico@gmail.com>
2021-06-13 20:36:29 +0800

    Remove create method from PendingQuoteRepository

--- src/domain/pending-quote/pending-quote.repository.ts
+++ src/domain/pending-quote/pending-quote.repository.ts
@@ -1,7 +1,4 @@
 import { Repository } from '../repository.abstract'
 import { PendingQuote } from './pending-quote.entity'
-import { IQuoteToSubmit } from './quote-to-submit.interface'
 
-export abstract class PendingQuoteRepository extends Repository<PendingQuote> {
-  abstract create(quote: IQuoteToSubmit): Promise<PendingQuote>
-}
+export abstract class PendingQuoteRepository extends Repository<PendingQuote> {}
