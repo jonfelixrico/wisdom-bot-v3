c178c92e9f9ddaa90c284115b1797f70d24b4e64
Felix <jonfelixrico@gmail.com>
2021-06-13 21:09:01 +0800

    Implement ReceiveRepository functionality

--- src/repositories/receive-repository/receive-repository.service.ts
+++ src/repositories/receive-repository/receive-repository.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common'
+import { Receive } from 'src/domain/receive/receive.entity'
+import { ReceiveRepository } from 'src/domain/receive/receive.repository'
+import { ReceiveDbEntity } from 'src/typeorm/entities/receive.typeorm-entity'
+import { Repository } from 'typeorm'
+import { IInteraction } from 'src/domain/receive/receive.entity'
 
 @Injectable()
-export class ReceiveRepositoryService {}
+export class ReceiveRepositoryService extends ReceiveRepository {
+  constructor(private repo: Repository<ReceiveDbEntity>) {
+    super()
+  }
+
+  async findById(receiveId: string): Promise<Receive> {
+    const receiveEnt = await this.repo.findOne(
+      { id: receiveId },
+      { relations: ['concurs'] },
+    )
+
+    if (!receiveEnt) {
+      return null
+    }
+
+    const concurEnts = await receiveEnt.concurs
+
+    const interactions = concurEnts.map<IInteraction>(
+      ({ concurDt: interactDt, id: interactionId, userId, karma }) => ({
+        interactDt,
+        interactionId,
+        karma,
+        userId,
+      }),
+    )
+
+    const { channelId, messageId, quoteId } = receiveEnt
+
+    return new Receive({
+      channelId,
+      interactions,
+      messageId,
+      quoteId,
+      receiveId,
+    })
+  }
+}
