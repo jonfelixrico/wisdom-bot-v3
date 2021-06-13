2ec121715f71e1e0ebc42f12c1317ec4748e0a5a
Felix <jonfelixrico@gmail.com>
2021-06-13 20:13:06 +0800

    Implement functionality to the QuoteRepositoryService

--- src/repositories/quote-repository/quote-repository.service.ts
+++ src/repositories/quote-repository/quote-repository.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common'
+import { Quote, IQuoteReceive } from 'src/domain/quote/quote.entity'
+import { QuoteRepository } from 'src/domain/quote/quote.repository'
+import { Repository } from 'typeorm'
+import { QuoteDbEntity } from 'src/typeorm/entities/quote.typeorm-entity'
 
 @Injectable()
-export class QuoteRepositoryService {}
+export class QuoteRepositoryService extends QuoteRepository {
+  constructor(private quoteTr: Repository<QuoteDbEntity>) {
+    super()
+  }
+
+  async findById(id: string): Promise<Quote> {
+    const quoteEnt = await this.quoteTr.findOne(
+      { id },
+      { relations: ['receives'] },
+    )
+
+    if (!quoteEnt) {
+      return null
+    }
+
+    const receiveEnts = await quoteEnt.receives
+
+    const receives = receiveEnts.map<IQuoteReceive>(
+      ({ id: receiveId, receiveDt, userId }) => ({
+        receiveId,
+        receiveDt,
+        userId,
+      }),
+    )
+
+    const {
+      approveDt: acceptDt,
+      authorId,
+      content,
+      guildId,
+      id: quoteId,
+      submitDt,
+      submitterId,
+    } = quoteEnt
+
+    return new Quote({
+      acceptDt,
+      authorId,
+      content,
+      guildId,
+      quoteId,
+      submitDt,
+      submitterId,
+      receives,
+    })
+  }
+}
