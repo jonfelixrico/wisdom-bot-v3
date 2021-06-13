e8f84756c96eda38377b89e593d509ea00cf92c3
Felix <jonfelixrico@gmail.com>
2021-06-13 19:24:49 +0800

    Add logging to the domain event handler

--- src/event-handlers/generic-domain-event-handler/generic-domain-event-handler.service.ts
+++ src/event-handlers/generic-domain-event-handler/generic-domain-event-handler.service.ts
@@ -1,12 +1,18 @@
 import { EventStoreDBClient, jsonEvent } from '@eventstore/db-client'
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
 import { Event } from 'src/domain/event.abstract'
+import { Inject, Logger } from '@nestjs/common'
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
 
 @EventsHandler()
 export class GenericDomainEventHandlerService
   implements IEventHandler<Event<unknown>>
 {
-  constructor(private esdbClient: EventStoreDBClient) {}
+  constructor(
+    private esdbClient: EventStoreDBClient,
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private logger: Logger,
+  ) {}
 
   async handle(event: Event<unknown>) {
     if (!(event instanceof Event)) {
@@ -22,5 +28,9 @@ export class GenericDomainEventHandlerService
     })
 
     await this.esdbClient.appendToStream(aggregateId, [esdbEvent])
+    this.logger.debug(
+      `Processed event ${eventName} for stream ${aggregateId}`,
+      GenericDomainEventHandlerService.name,
+    )
   }
 }
