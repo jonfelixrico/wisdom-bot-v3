58d411c954b25916846a10732fb2f946a4674f25
Felix <jonfelixrico@gmail.com>
2021-06-15 18:50:19 +0800

    Revert "Define and implement create method in the PendingQuoteRepositoryService"
    This reverts commit bf40f3a03a179d1415a3d045881994fcf30e5ff5.

--- src/domain/pending-quote/pending-quote.repository.ts
+++ src/domain/pending-quote/pending-quote.repository.ts
@@ -1,7 +1,4 @@
 import { Repository } from '../repository.abstract'
 import { PendingQuote } from './pending-quote.entity'
-import { IQuoteToSubmit } from 'src/domain/pending-quote/quote-to-submit.interface'
 
-export abstract class PendingQuoteRepository extends Repository<PendingQuote> {
-  abstract create(quoteToSubmit: IQuoteToSubmit): Promise<PendingQuote>
-}
+export abstract class PendingQuoteRepository extends Repository<PendingQuote> {}
