9f53b074a23b11857b6964d66b7cfb21b1cfad08
Felix <jonfelixrico@gmail.com>
2021-06-15 20:41:36 +0800

    Change SubmitQuoteCommandHandler implementation

--- src/command-handlers/pending-quote/submit-quote-command-handler/submit-quote-command-handler.service.ts
+++ src/command-handlers/pending-quote/submit-quote-command-handler/submit-quote-command-handler.service.ts
@@ -1,19 +1,29 @@
-import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs'
-import { PendingQuoteRepository } from 'src/domain/pending-quote/pending-quote.repository'
+import { Logger } from '@nestjs/common'
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs'
+import { QuoteSubmitted } from 'src/domain/pending-quote/quote-submitted.event'
 import { SubmitQuoteCommand } from 'src/domain/pending-quote/submit-quote.command'
+import { v4 } from 'uuid'
 
 @CommandHandler(SubmitQuoteCommand)
 export class SubmitQuoteCommandHandlerService
   implements ICommandHandler<SubmitQuoteCommand>
 {
-  constructor(
-    private publisher: EventPublisher,
-    private repo: PendingQuoteRepository,
-  ) {}
+  constructor(private bus: EventBus, private logger: Logger) {}
 
   async execute({ payload }: SubmitQuoteCommand): Promise<any> {
-    const entity = await this.repo.create(payload)
-    const pendingQuote = this.publisher.mergeObjectContext(entity)
-    pendingQuote.commit()
+    const quoteId = v4()
+    const event = new QuoteSubmitted({
+      ...payload,
+      quoteId,
+      acceptDt: null,
+      cancelDt: null,
+    })
+
+    this.bus.publish(event)
+
+    this.logger.debug(
+      `Created quote ${quoteId}.`,
+      SubmitQuoteCommandHandlerService.name,
+    )
   }
 }
