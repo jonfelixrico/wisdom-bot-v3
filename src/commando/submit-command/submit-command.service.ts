3ec18d782452f964505b2299f974861d084b0747
Felix <jonfelixrico@gmail.com>
2021-06-15 18:42:17 +0800

    Attempt to CQRS-ify the SubmitCommand commando service

--- src/commando/submit-command/submit-command.service.ts
+++ src/commando/submit-command/submit-command.service.ts
@@ -6,11 +6,10 @@ import {
   CommandoMessage,
 } from 'discord.js-commando'
 import { GuildRepository } from 'src/classes/guild-repository.abstract'
-import { PendingQuoteRepository } from 'src/classes/pending-quote-repository.abstract'
-import { DeleteListenerService } from '../delete-listener/delete-listener.service'
-import { ReactionListenerService } from '../reaction-listener/reaction-listener.service'
 import { IArgumentMap, WrappedCommand } from '../wrapped-command.class'
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
+import { CommandBus } from '@nestjs/cqrs'
+import { SubmitQuoteCommand } from 'src/domain/pending-quote/submit-quote.command'
 
 const COMMAND_INFO: CommandInfo = {
   name: 'submit',
@@ -43,12 +42,10 @@ interface ISubmitCommandArgs extends IArgumentMap {
 export class SubmitCommandService extends WrappedCommand<ISubmitCommandArgs> {
   constructor(
     client: CommandoClient,
-    private pendingRepo: PendingQuoteRepository,
     private guildRepo: GuildRepository,
-    private reactionListener: ReactionListenerService,
-    private deleteListener: DeleteListenerService,
     @Inject(WINSTON_MODULE_NEST_PROVIDER)
     private readonly logger: Logger,
+    private commandBus: CommandBus,
   ) {
     super(client, COMMAND_INFO)
   }
@@ -58,45 +55,37 @@ export class SubmitCommandService extends WrappedCommand<ISubmitCommandArgs> {
     { quote, author }: ISubmitCommandArgs,
   ): Promise<Message | Message[]> {
     const guildId = message.guild.id
+    const channelId = message.channel.id
+    const submitterId = message.author.id
+
     const { expireMillis, approveEmoji, approveCount } =
       await this.guildRepo.getQuoteSettings(guildId)
 
     const expireDt = new Date(Date.now() + expireMillis)
 
-    const quoteLine = `**"${quote}"** - ${author}, ${new Date().getFullYear()}`
-    const instructionsLine = `_This submission needs ${
-      approveCount + 1
-    } ${approveEmoji} reacts to get reactions on or before *${expireDt}*._`
+    const response = await message.channel.send('Loading...')
 
-    const response = await message.channel.send('🤔')
-    await response.edit([quoteLine, instructionsLine].join('\n'))
-
-    const { quoteId } = await this.pendingRepo.createPendingQuote({
+    const command = new SubmitQuoteCommand({
       authorId: author.id,
-      submitterId: message.author.id,
-      channelId: message.channel.id,
+      channelId,
       content: quote,
-      messageId: response.id,
-      guildId,
       expireDt,
-      approvalCount: approveCount,
-      approvalEmoji: approveEmoji,
+      guildId,
+      messageId: response.id,
+      submitDt: new Date(),
+      submitterId,
+      upvoteCount: approveCount,
+      upvoteEmoji: approveEmoji,
     })
 
+    const res = await this.commandBus.execute(command)
+    console.log(res)
+
     this.logger.log(
-      `Created quote ${quoteId} in guild ${guildId}`,
+      `User ${submitterId} submitted a quote in channel ${channelId} at guild ${guildId}.`,
       SubmitCommandService.name,
     )
 
-    this.reactionListener.watch(
-      response.id,
-      approveEmoji,
-      approveCount,
-      expireDt,
-    )
-    this.deleteListener.watch(response.id)
-
-    await response.react(approveEmoji)
     return response
   }
 }
