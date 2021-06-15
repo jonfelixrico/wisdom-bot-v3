1674661288a085dd9482c43d39f79c146c763d1e
Felix <jonfelixrico@gmail.com>
2021-06-15 19:25:18 +0800

    Add CqrsModule to the imports of EventHandlersModule

--- src/event-handlers/event-handlers.module.ts
+++ src/event-handlers/event-handlers.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
+import { CqrsModule } from '@nestjs/cqrs'
 import { EventSourcingModule } from 'src/event-sourcing/event-sourcing.module'
 import { GenericDomainEventHandlerService } from './generic-domain-event-handler/generic-domain-event-handler.service'
 
 @Module({
-  imports: [EventSourcingModule],
+  imports: [CqrsModule, EventSourcingModule],
   providers: [GenericDomainEventHandlerService],
 })
 export class EventHandlersModule {}
