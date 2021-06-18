1123133c24503ca80567e2771176d8fd81f48bed
Felix <jonfelixrico@gmail.com>
2021-06-18 22:42:06 +0800

    Move over the reducer code into PendingQuoteWriteRepositoryService

--- src/write-repositories/pending-quote-write-repository/pending-quote-write-repository.service.ts
+++ src/write-repositories/pending-quote-write-repository/pending-quote-write-repository.service.ts
@@ -1,21 +1,66 @@
 import { Injectable } from '@nestjs/common'
+import { DomainEventNames } from 'src/domain/domain-event-names.enum'
 import { PendingQuote } from 'src/domain/pending-quote/pending-quote.entity'
-import { PendingQuoteRepository } from 'src/domain/pending-quote/pending-quote.repository'
-import { ReadStreamService } from 'src/event-store/read-stream/read-stream.service'
-import { pendingQuoteReducer } from './pending-quote.reducer'
+import {
+  ReadStreamService,
+  Reducer,
+} from 'src/event-store/read-stream/read-stream.service'
+import { PendingQuoteEsdbRepository } from '../abstract/pending-quote-esdb-repository.abstract'
+import { IPendingQuote } from 'src/domain/pending-quote/pending-quote.interface'
+import { IPendingQuoteCancelledPayload } from 'src/domain/pending-quote/pending-quote-cancelled.event'
+import { ResolvedEvent } from '@eventstore/db-client'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function quoteSubmitted(state: IPendingQuote, data: IPendingQuote) {
+  return data
+}
+
+function quoteCancelled(
+  state: IPendingQuote,
+  { cancelDt }: IPendingQuoteCancelledPayload,
+) {
+  state.cancelDt = cancelDt
+  return state
+}
+
+const { PENDING_QUOTE_ACCEPTED, PENDING_QUOTE_CANCELLED, QUOTE_SUBMITTED } =
+  DomainEventNames
+
+const ReducerMapping = {
+  [PENDING_QUOTE_CANCELLED]: quoteCancelled,
+  [QUOTE_SUBMITTED]: quoteSubmitted,
+}
+
+export const pendingQuoteReducer: Reducer<IPendingQuote> = (
+  state: IPendingQuote,
+  { event }: ResolvedEvent,
+  next,
+  { stop },
+) => {
+  const { type, data } = event
+  if (type === PENDING_QUOTE_ACCEPTED) {
+    stop(null)
+    return
+  }
+
+  next(ReducerMapping[type](state, data))
+}
 
 @Injectable()
-export class PendingQuoteWriteRepositoryService extends PendingQuoteRepository {
+export class PendingQuoteWriteRepositoryService extends PendingQuoteEsdbRepository {
   constructor(private readStream: ReadStreamService) {
     super()
   }
 
-  async findById(id: string): Promise<PendingQuote> {
-    const { state } = await this.readStream.readStreamFromBeginning(
+  async findById(id: string) {
+    const { state, revision } = await this.readStream.readStreamFromBeginning(
       `quote-${id}`,
       pendingQuoteReducer,
     )
 
-    return new PendingQuote(state)
+    return {
+      entity: new PendingQuote(state),
+      revision,
+    }
   }
 }
