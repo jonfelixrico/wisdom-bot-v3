522e49ec5bd086c5992fc9ea13f7a1a0405c0a97
Felix <jonfelixrico@gmail.com>
2021-06-20 00:38:09 +0800

    Make EsdbLiveSubscriptionEvent generic

--- src/event-store/esdb-event-publisher/esdb-live-subscription.event.ts
+++ src/event-store/esdb-event-publisher/esdb-live-subscription.event.ts
@@ -1,15 +1,15 @@
 import { AllStreamRecordedEvent, JSONType } from '@eventstore/db-client'
 import { IEvent } from '@nestjs/cqrs'
 
-export class EsdbLiveSubscriptionEvent implements IEvent {
+export class EsdbLiveSubscriptionEvent<DataType = JSONType> implements IEvent {
   readonly streamId: string
-  readonly data: JSONType
+  readonly data: DataType
   readonly revision: bigint
   readonly type: string
 
   constructor(esdbEvent: AllStreamRecordedEvent) {
     const { streamId, type, data, revision } = esdbEvent
-    this.data = data as JSONType
+    this.data = data as DataType
     this.revision = revision
     this.type = type
     this.streamId = streamId
