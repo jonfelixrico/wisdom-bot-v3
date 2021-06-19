ffef03a1f1b10b55be25d0de61d33288ece33f16
Felix <jonfelixrico@gmail.com>
2021-06-20 00:18:05 +0800

    Add logging to EsdbEventPublisherService

--- src/event-store/esdb-event-publisher/esdb-event-publisher.service.ts
+++ src/event-store/esdb-event-publisher/esdb-event-publisher.service.ts
@@ -1,25 +1,41 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { END, EventStoreDBClient } from '@eventstore/db-client'
 import { EventBus } from '@nestjs/cqrs'
 import { EsdbLiveSubscriptionEvent } from './esdb-live-subscription.event'
 
 @Injectable()
 export class EsdbEventPublisherService {
-  constructor(private client: EventStoreDBClient, private bus: EventBus) {
+  constructor(
+    private client: EventStoreDBClient,
+    private bus: EventBus,
+    private logger: Logger,
+  ) {
     this.onInit()
   }
 
   onInit() {
+    const { logger } = this
     this.client
       .subscribeToAll({ fromPosition: END })
       .on('data', ({ event }) => {
-        const { isJson } = event
+        const { isJson, type, id, streamId } = event
 
         if (!isJson) {
-          return
+          logger.debug(
+            `Event ${id} is not JSON, skipped.`,
+            EsdbEventPublisherService.name,
+          )
+        } else if (type.startsWith('$')) {
+          logger.debug(
+            `Event ${id} is a system event (${type}), skipped.`,
+            EsdbEventPublisherService.name,
+          )
+        } else {
+          logger.debug(
+            `Received event ${id} from stream ${streamId} of type ${type}.`,
+          )
+          this.bus.publish(new EsdbLiveSubscriptionEvent(event))
         }
-
-        this.bus.publish(new EsdbLiveSubscriptionEvent(event))
       })
   }
 }
