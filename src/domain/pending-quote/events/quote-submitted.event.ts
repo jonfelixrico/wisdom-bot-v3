64be168f132b8692aa412cc8627b2222bba914f8
Felix <jonfelixrico@gmail.com>
2021-06-19 22:28:48 +0800

    Rename QuoteSubmitted class to QuoteSubmittedEvent

--- src/domain/pending-quote/events/quote-submitted.event.ts
+++ src/domain/pending-quote/events/quote-submitted.event.ts
@@ -2,7 +2,7 @@ import { DomainEventNames } from 'src/domain/domain-event-names.enum'
 import { IPendingQuote } from 'src/domain/pending-quote/pending-quote.interface'
 import { BasePendingQuoteEvent } from './base-pending-quote-event.abstract'
 
-export class QuoteSubmitted extends BasePendingQuoteEvent<IPendingQuote> {
+export class QuoteSubmittedEvent extends BasePendingQuoteEvent<IPendingQuote> {
   constructor(created: IPendingQuote) {
     super(DomainEventNames.QUOTE_SUBMITTED, created.quoteId, created)
   }
