5b6417f0511ea719338e22788eceae1b03dfd34f
Felix <jonfelixrico@gmail.com>
2021-06-19 21:12:57 +0800

    Move entity and event abstracts into a dedicated folder

---
+++ src/domain/abstracts/domain-entity.abstract.ts
@@ -0,0 +1,15 @@
+import { DomainEvent } from './domain-event.abstract'
+
+export abstract class DomainEntity<
+  EventType extends DomainEvent = DomainEvent,
+> {
+  private _events: EventType[]
+
+  apply(event: EventType) {
+    this._events.push(event)
+  }
+
+  get events() {
+    return [...this._events]
+  }
+}
