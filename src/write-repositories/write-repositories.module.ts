9b8b35149589c4110a62cbd82771ff93bc456a7e
Felix <jonfelixrico@gmail.com>
2021-06-19 20:45:44 +0800

    Use PendingQuoteEsdbRepository as PendignQuoteWriteRepositoryService's provider token

--- src/write-repositories/write-repositories.module.ts
+++ src/write-repositories/write-repositories.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common'
 import { QuoteWriteRepositoryService } from './quote-write-repository/quote-write-repository.service'
 import { PendingQuoteWriteRepositoryService } from './pending-quote-write-repository/pending-quote-write-repository.service'
 import { ReceiveWriteRepositoryService } from './receive-write-repository/receive-write-repository.service'
+import { PendingQuoteEsdbRepository } from './abstract/pending-quote-esdb-repository.abstract'
 
 @Module({
   providers: [
+    {
+      useClass: PendingQuoteWriteRepositoryService,
+      provide: PendingQuoteEsdbRepository,
+    },
     QuoteWriteRepositoryService,
-    PendingQuoteWriteRepositoryService,
     ReceiveWriteRepositoryService,
   ],
 })
