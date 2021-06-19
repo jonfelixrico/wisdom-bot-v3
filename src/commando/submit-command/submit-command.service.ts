8329ed6170011a9be4c12b6c624898e788c64941
Felix <jonfelixrico@gmail.com>
2021-06-19 22:52:44 +0800

    Relocate command info for SubmitCommandService into an external file

--- src/commando/submit-command/submit-command.service.ts
+++ src/commando/submit-command/submit-command.service.ts
@@ -1,39 +1,13 @@
-import { Inject, Injectable, Logger } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { Message, User } from 'discord.js'
-import {
-  CommandInfo,
-  CommandoClient,
-  CommandoMessage,
-} from 'discord.js-commando'
+import { CommandoClient, CommandoMessage } from 'discord.js-commando'
 import { GuildRepository } from 'src/classes/guild-repository.abstract'
 import { IArgumentMap, WrappedCommand } from '../wrapped-command.class'
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
 import { CommandBus } from '@nestjs/cqrs'
 import { SubmitQuoteCommand } from 'src/domain/pending-quote/submit-quote.command'
 import { ReactionListenerService } from '../reaction-listener/reaction-listener.service'
 import { DeleteListenerService } from '../delete-listener/delete-listener.service'
-
-const COMMAND_INFO: CommandInfo = {
-  name: 'submit',
-  aliases: ['add'],
-  group: 'commands',
-  memberName: 'submit',
-  description:
-    'Submit a new quote. This quote has to go through an approval process.',
-  args: [
-    {
-      key: 'author',
-      type: 'user',
-      prompt: '',
-    },
-    {
-      key: 'quote',
-      type: 'string',
-      prompt: '',
-    },
-  ],
-  argsPromptLimit: 0,
-}
+import { SUBMIT_COMMAND_INFO } from './submit-command-info'
 
 interface ISubmitCommandArgs extends IArgumentMap {
   author: User
@@ -49,7 +23,7 @@ export class SubmitCommandService extends WrappedCommand<ISubmitCommandArgs> {
     private reactionListener: ReactionListenerService,
     private deleteListener: DeleteListenerService,
   ) {
-    super(client, COMMAND_INFO)
+    super(client, SUBMIT_COMMAND_INFO)
   }
 
   async run(
