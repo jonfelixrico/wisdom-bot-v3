4a4308fc97ce8e027771c1cf13742064ea704fb7
Felix <jonfelixrico@gmail.com>
2021-06-20 19:18:59 +0800

    Add comments to ReadRepositoryEsdbEventService

--- src/read-repositories/read-repository-esdb.event.ts
+++ src/read-repositories/read-repository-esdb.event.ts
@@ -6,7 +6,18 @@ interface IStreamEvent<DataType = JSONType> {
   readonly type: string
 }
 
+/**
+ * This class represents a series of events from a specific stream.
+ * This is meant to be the only events read models should listen to when building
+ * their data.
+ */
 export class ReadRepositoryEsdbEvent {
+  /**
+   * @param streamId The id of the stream the `events` are from.
+   * @param events Contains event data, revision, and type. They are expected to be in-sequence.
+   * @param source `LIVE` if this event was relayed from a live event, `CATCH_UP` if this was a
+   *   response for a catch-up query.
+   */
   constructor(
     readonly streamId: string,
     readonly events: IStreamEvent<unknown>[],
