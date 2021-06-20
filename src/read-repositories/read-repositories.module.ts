4bc8796c7cddeeba120fcc8324688cec247094da
Felix <jonfelixrico@gmail.com>
2021-06-20 18:20:33 +0800

    Include new imports to the ReadRepositories module

--- src/read-repositories/read-repositories.module.ts
+++ src/read-repositories/read-repositories.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { CqrsModule } from '@nestjs/cqrs'
 import { EventStoreModule } from 'src/event-store/event-store.module'
+import { TypeormModule } from 'src/typeorm/typeorm.module'
 import { GetPendingQuoteByMessageIdQueryHandlerService } from './handlers/get-pending-quote-by-message-id-query-handler/get-pending-quote-by-message-id-query-handler.service'
 
 @Module({
+  imports: [EventStoreModule, TypeormModule, CqrsModule],
   providers: [GetPendingQuoteByMessageIdQueryHandlerService],
-
-  imports: [EventStoreModule],
 })
 export class ReadRepositoriesModule {}
