70d9018824f2ba809ca0373133107f1844a63f0a
Felix <jonfelixrico@gmail.com>
2021-06-20 19:01:48 +0800

    Rename EsdbEventPublisherService to EsdbLiveEventRelayService

--- src/event-store/esdb-event-publisher/esdb-live-event-relay.service.ts
+++ src/event-store/esdb-event-publisher/esdb-live-event-relay.service.ts
@@ -4,16 +4,22 @@ import { EventBus } from '@nestjs/cqrs'
 import { EsdbLiveEvent } from './esdb-live.event'
 
 @Injectable()
-export class EsdbEventPublisherService {
+/**
+ * The responsibility of this service is to listen to ESDB's "$all" stream and relay it
+ * to the entire app via NestJS' CQRS event bus.
+ *
+ * Non-JSON events or system events are not relayed.
+ */
+export class EsdbLiveEventRelayService {
   constructor(
     private client: EventStoreDBClient,
     private bus: EventBus,
     private logger: Logger,
   ) {
-    this.onInit()
+    this.initiateSubscription()
   }
 
-  onInit() {
+  private initiateSubscription() {
     const { logger } = this
     this.client
       .subscribeToAll({ fromPosition: END })
@@ -23,17 +29,17 @@ export class EsdbEventPublisherService {
         if (type.startsWith('$')) {
           logger.debug(
             `Skipped event ${id} from stream ${streamId}; Reason: system event (${type})`,
-            EsdbEventPublisherService.name,
+            EsdbLiveEventRelayService.name,
           )
         } else if (!isJson) {
           logger.debug(
             `Skipped event ${id} from stream ${streamId}; Reason: not JSON`,
-            EsdbEventPublisherService.name,
+            EsdbLiveEventRelayService.name,
           )
         } else {
           logger.debug(
             `Relayed event ${id} from stream ${streamId} of type ${type}`,
-            EsdbEventPublisherService.name,
+            EsdbLiveEventRelayService.name,
           )
           this.bus.publish(new EsdbLiveEvent(event))
         }
--- src/event-store/event-store.module.ts
+++ src/event-store/event-store.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common'
 import { eventStoreClientProvider } from './event-store-client.provider'
 import { ReadStreamService } from './read-stream/read-stream.service'
-import { EsdbEventPublisherService } from './esdb-event-publisher/esdb-event-publisher.service'
+import { EsdbLiveEventRelayService } from './esdb-event-publisher/esdb-live-event-relay.service'
 import { CqrsModule } from '@nestjs/cqrs'
 
 const exportedProviders = [eventStoreClientProvider, ReadStreamService]
 
 @Module({
   exports: [...exportedProviders],
-  providers: [...exportedProviders, EsdbEventPublisherService],
+  providers: [
+    ...exportedProviders,
+    /*
+     * We're not going to export the live stream service because it doesn't have
+     * any exposed methods. It's meant to only publish events.
+     */
+    EsdbLiveEventRelayService,
+  ],
   imports: [CqrsModule],
 })
 export class EventStoreModule {}
