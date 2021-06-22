2dd60e8f5c027f390ee666fa92f61dec031cea40
Felix <jonfelixrico@gmail.com>
2021-06-22 19:08:49 +0800

    Remove instances of CatchUpQuery

--- src/read-repositories/esdb-catch-up.query.ts
+++
@@ -1,9 +0,0 @@
-import { ReadRevision } from '@eventstore/db-client'
-import { IQuery } from '@nestjs/cqrs'
-
-export class EsdbCatchUpQuery implements IQuery {
-  constructor(
-    readonly streamName: string,
-    readonly fromRevision: ReadRevision,
-  ) {}
-}
