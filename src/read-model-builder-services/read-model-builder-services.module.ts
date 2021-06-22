e67c5ebc522d913825ed7bd72b249ba2fc03b17d
Felix <jonfelixrico@gmail.com>
2021-06-23 01:52:58 +0800

    Implement GetEventQueryHandler

--- src/read-model-builder-services/read-model-builder-services.module.ts
+++ src/read-model-builder-services/read-model-builder-services.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { CqrsModule } from '@nestjs/cqrs'
 import { EventStoreModule } from 'src/event-store/event-store.module'
+import { GetEventQueryHandlerService } from './get-event-query-handler/get-event-query-handler.service'
 
 @Module({
   imports: [EventStoreModule, CqrsModule],
+  providers: [GetEventQueryHandlerService],
 })
 export class ReadModelBuilderServicesModule {}
