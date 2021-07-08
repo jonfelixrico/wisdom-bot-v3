76bdb27dc1eb1537f08530027ff3336a98f2beb2
Felix <jonfelixrico@gmail.com>
2021-07-08 19:46:38 +0800

    Create boilerplate code for catching up the quote entity

--- src/read-repositories/read-repositories.module.ts
+++ src/read-repositories/read-repositories.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common'
+import { QuoteCatchUpService } from './quote-catch-up/quote-catch-up.service'
 
-@Module({})
+@Module({
+  providers: [QuoteCatchUpService],
+})
 export class ReadRepositoriesModule {}
