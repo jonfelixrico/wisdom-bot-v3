cade1fb0dd8c28d9f9f3b0372733b8ef9b1948bf
Felix <jonfelixrico@gmail.com>
2021-07-09 01:01:34 +0800

    Import the necessary modules in ReadRepositoriesModule

--- src/read-repositories/read-repositories.module.ts
+++ src/read-repositories/read-repositories.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { QuoteCatchUpService } from './catch-up/quote-catch-up/quote-catch-up.service'
 import { CatchUpOrchestratorService } from './catch-up/catch-up-orchestrator/catch-up-orchestrator.service'
+import { TypeormModule } from 'src/typeorm/typeorm.module'
+import { EventStoreModule } from 'src/event-store/event-store.module'
 
 @Module({
   providers: [QuoteCatchUpService, CatchUpOrchestratorService],
+  imports: [TypeormModule, EventStoreModule],
 })
 export class ReadRepositoriesModule {}
