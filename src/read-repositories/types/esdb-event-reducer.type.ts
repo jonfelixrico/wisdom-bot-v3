b7761f9dd3a4d3c60b579507c4b23f3ea8fac9c2
Felix <jonfelixrico@gmail.com>
2021-07-08 20:21:27 +0800

    Make the reducer function type generic

--- src/read-repositories/types/esdb-event-reducer.type.ts
+++ src/read-repositories/types/esdb-event-reducer.type.ts
@@ -1,7 +1,15 @@
-import { JSONRecordedEvent } from '@eventstore/db-client'
+import {
+  JSONEventType,
+  JSONRecordedEvent,
+  JSONType,
+} from '@eventstore/db-client'
 import { EntityManager } from 'typeorm'
 
-export type EsdbEventReducer = (
-  event: JSONRecordedEvent,
+type ExtendedJsonType = JSONType | { [key: string]: any }
+
+export type EsdbEventReducer<
+  EventDataType extends ExtendedJsonType = ExtendedJsonType,
+> = (
+  event: JSONRecordedEvent<JSONEventType<string, EventDataType, unknown>>,
   transaction: EntityManager,
 ) => Promise<void>
