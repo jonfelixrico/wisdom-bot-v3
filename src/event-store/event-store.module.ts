90b5c8b2308239df782a01b3369bccbbc2ede14e
Felix <jonfelixrico@gmail.com>
2021-07-08 23:20:35 +0800

    Create service for iterating through all the events of a stream

--- src/event-store/event-store.module.ts
+++ src/event-store/event-store.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 import { eventStoreClientProvider } from './event-store-client.provider'
 import { EsdbLiveEventRelayService } from './esdb-event-publisher/esdb-live-event-relay.service'
 import { CqrsModule } from '@nestjs/cqrs'
+import { StreamReaderService } from './stream-reader/stream-reader.service'
 
-const exportedProviders = [eventStoreClientProvider]
+const exportedProviders = [eventStoreClientProvider, StreamReaderService]
 
 @Module({
-  exports: [...exportedProviders],
+  exports: exportedProviders,
   providers: [
     ...exportedProviders,
     /*
---
+++ src/event-store/stream-reader/stream-reader.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common'
+import {
+  EventStoreDBClient,
+  ReadRevision,
+  ResolvedEvent,
+  START,
+} from '@eventstore/db-client'
+import { ReadStreamOptions } from '@eventstore/db-client/dist/streams'
+
+const DEFAULT_MAX_COUNT = 50
+
+export type ReadStreamIterator = (resolvedEvent: ResolvedEvent) => Promise<any>
+
+/**
+ * This is pretty much a paginated version of `EventStoreDBClient#readStream`.
+ * It's easier to explain why we would need this instead of explaining what it is:
+ *
+ * For example, you want to read the stream which contains tens of thousands of events, or anything with
+ * an absurd amount. If you load all of that, there's a risk where you'll run out of memory since they're
+ * too many.
+ *
+ * A smarter approach to do to avoid the memory problem is to gradually just load the events until
+ * you've reached the end of the stream. This service serves as the framework to make the smarter approach more
+ * convenient to utilize again and again.
+ */
+@Injectable()
+export class StreamReaderService {
+  constructor(private client: EventStoreDBClient) {}
+
+  async readStream(
+    streamName: string,
+    options: ReadStreamOptions = {},
+    iterator: ReadStreamIterator,
+  ): Promise<void> {
+    let fromRevision: ReadRevision = options.fromRevision ?? START
+    const countQuota = options.maxCount ?? DEFAULT_MAX_COUNT
+
+    while (true) {
+      const resolvedEvents = await this.client.readStream(streamName, {
+        ...options,
+        fromRevision,
+      })
+
+      if (!resolvedEvents.length) {
+        return
+      }
+
+      for (const resolvedEvent of resolvedEvents) {
+        await iterator(resolvedEvent)
+      }
+
+      if (resolvedEvents.length < countQuota) {
+        return
+      }
+
+      fromRevision =
+        resolvedEvents[resolvedEvents.length - 1].event.revision + 1n
+    }
+  }
+}
