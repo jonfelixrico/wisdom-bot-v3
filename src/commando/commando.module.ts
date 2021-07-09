d321f286a7ec7573ed2bbbdd57dc2060f9f89853
Felix <jonfelixrico@gmail.com>
2021-07-10 03:21:33 +0800

    Import ReadRepositoriesModue in CommandModule

--- src/commando/commando.module.ts
+++ src/commando/commando.module.ts
@@ -7,15 +7,18 @@ import { QuoteApproverService } from './quote-approver/quote-approver.service'
 import { DeleteListenerService } from './delete-listener/delete-listener.service'
 import { CqrsModule } from '@nestjs/cqrs'
 import { TypeormModule } from 'src/typeorm/typeorm.module'
+import { ReceiveCommandService } from './receive-command/receive-command.service'
+import { ReadRepositoriesModule } from 'src/read-repositories/read-repositories.module'
 
 @Module({
-  imports: [DiscordModule, CqrsModule, TypeormModule],
+  imports: [DiscordModule, CqrsModule, TypeormModule, ReadRepositoriesModule],
   providers: [
     SetupService,
     SubmitCommandService,
     ReactionListenerService,
     QuoteApproverService,
     DeleteListenerService,
+    ReceiveCommandService,
   ],
 })
 export class CommandoModule {}
