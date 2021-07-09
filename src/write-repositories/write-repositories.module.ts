7899455780f1b5ab68507e301166d1074d5d5925
Felix <jonfelixrico@gmail.com>
2021-07-10 03:20:44 +0800

    Export QuoteWriteRepositoryService

--- src/write-repositories/write-repositories.module.ts
+++ src/write-repositories/write-repositories.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common'
 import { PendingQuoteWriteRepositoryService } from './pending-quote-write-repository/pending-quote-write-repository.service'
 import { EventStoreModule } from 'src/event-store/event-store.module'
 import { ReadStreamService } from './read-stream/read-stream.service'
+import { QuoteWriteRepositoryService } from './quote-write-repository/quote-write-repository.service'
 
-const providersToExport = [PendingQuoteWriteRepositoryService]
+const providersToExport = [
+  PendingQuoteWriteRepositoryService,
+  QuoteWriteRepositoryService,
+]
 
 @Module({
   providers: [...providersToExport, ReadStreamService],
