a3d94b5d50e50abaf5f791c8991629f9e6e7c433
Felix <jonfelixrico@gmail.com>
2021-07-10 03:09:05 +0800

    Add receive-relevant code to QuoteQueryService

--- src/read-repositories/queries/quote-query/quote-query.service.ts
+++ src/read-repositories/queries/quote-query/quote-query.service.ts
@@ -6,24 +6,75 @@ import { Connection } from 'typeorm'
 export class QuoteQueryService {
   constructor(private conn: Connection) {}
 
-  private get manager() {
-    return this.conn.manager
-  }
-
   /**
    * Finds the id of the quote assocaited with the `messageId`
    * @param messageId
    * @returns Null if no associated quote was found; the id of the quote if otherwise.
    */
   async getQuoteIdFromMessageId(messageId: string) {
-    const matchingQuote = await this.manager.findOne(QuoteTypeormEntity, {
-      messageId,
-    })
+    const matchingQuote = await this.conn
+      .getRepository(QuoteTypeormEntity)
+      .findOne({
+        messageId,
+      })
 
     if (!matchingQuote) {
       return null
     }
 
     return matchingQuote.id
+  }
+
+  /**
+   * Gets the id of a random approved quote. If no quotes that matches the `guildId` and `authorId` criteria were found,
+   * then null will be returned.
+   *
+   * @param guildId
+   * @param authorId Optional.
+   * @returns Null if no quotes were found, else the id of a quote will be returned instead.
+   */
+  async getRandomQuoteId(guildId: string, authorId?: string) {
+    const query = this.conn
+      .getRepository(QuoteTypeormEntity)
+      .createQueryBuilder()
+      .orderBy('RAND()')
+
+    let result: QuoteTypeormEntity
+    if (authorId) {
+      result = await query
+        .where(
+          'approveDt IS NOT NULL AND guildId = :guildId AND authorId = :authorId',
+          {
+            guildId,
+            authorId,
+          },
+        )
+        .getOne()
+    } else {
+      result = await query
+        .where('approveDt IS NOT NULL AND guildId = :guildId', { guildId })
+        .getOne()
+    }
+
+    return result?.id ?? null
+  }
+
+  async getQuoteData(quoteId: string) {
+    const quote = await this.conn
+      .getRepository(QuoteTypeormEntity)
+      .findOne({ id: quoteId })
+
+    if (!quote) {
+      return null
+    }
+
+    const { authorId, content, submitDt } = quote
+
+    return {
+      quoteId,
+      content,
+      authorId,
+      year: submitDt.getFullYear(),
+    }
   }
 }
