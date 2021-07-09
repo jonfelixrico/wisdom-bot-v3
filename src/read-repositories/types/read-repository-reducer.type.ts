d13ca92ee74d588521d0a0187347d871bab1d75b
Felix <jonfelixrico@gmail.com>
2021-07-09 23:26:12 +0800

    Make ReadRepositoryReducer extend JSONType as the constraint for EventDataType

--- src/read-repositories/types/read-repository-reducer.type.ts
+++ src/read-repositories/types/read-repository-reducer.type.ts
@@ -5,14 +5,10 @@ import {
 } from '@eventstore/db-client'
 import { EntityManager } from 'typeorm'
 
-type ExtendedJsonType = JSONType | { [key: string]: any }
-
 /**
  * This should be used for the reducers for consuming events for the read repository.
  */
-export type ReadRepositoryReducer<
-  EventDataType extends ExtendedJsonType = ExtendedJsonType,
-> = (
+export type ReadRepositoryReducer<EventDataType extends JSONType = JSONType> = (
   // This is directly taken from EventStoreDB
   event: JSONRecordedEvent<JSONEventType<string, EventDataType, unknown>>,
   // This is what we'll use to do TypeORM operations
