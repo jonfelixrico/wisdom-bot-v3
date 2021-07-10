872e486db487c58678c4ff78f40066785b6ca358
Felix <jonfelixrico@gmail.com>
2021-07-10 19:29:24 +0800

    Create classes for domain errors

---
+++ src/domain/errors/domain-error.class.ts
@@ -0,0 +1,7 @@
+import { DomainErrorCodes } from './domain-error-codes.enum'
+
+export class DomainError extends Error {
+  constructor(readonly code: DomainErrorCodes, message: string) {
+    super(message)
+  }
+}
