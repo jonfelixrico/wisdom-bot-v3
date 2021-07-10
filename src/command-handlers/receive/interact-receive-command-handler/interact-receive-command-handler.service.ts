58f9a6974cf22b10c07597e3e73f2378bfdc059e
Felix <jonfelixrico@gmail.com>
2021-07-10 19:18:15 +0800

    Implement logic for InteractReceiveCommandHandlerService

--- src/command-handlers/receive/interact-receive-command-handler/interact-receive-command-handler.service.ts
+++ src/command-handlers/receive/interact-receive-command-handler/interact-receive-command-handler.service.ts
@@ -1,13 +1,36 @@
-import { EventStoreDBClient } from '@eventstore/db-client'
+import { Logger } from '@nestjs/common'
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
 import { InteractReceiveCommand } from 'src/domain/commands/interact-receive.command'
+import { ReceiveWriteRepositoryService } from 'src/write-repositories/receive-write-repository/receive-write-repository.service'
 
 @CommandHandler(InteractReceiveCommand)
 export class InteractReceiveCommandHandlerService
   implements ICommandHandler<InteractReceiveCommand>
 {
+  constructor(
+    private repo: ReceiveWriteRepositoryService,
+    private logger: Logger,
+  ) {}
+
   async execute({ payload }: InteractReceiveCommand): Promise<any> {
     const { karma, receiveId, userId } = payload
-    throw new Error('Method not implemented.')
+
+    const receive = await this.repo.findById(receiveId)
+    if (!receive) {
+      this.logger.verbose(
+        `Did not process InteractReceiveCommand for receive ${receiveId} not found.`,
+        InteractReceiveCommandHandlerService.name,
+      )
+      return null
+    }
+
+    const { entity, revision } = receive
+    entity.interact({ karma, userId })
+
+    this.repo.publishEvents(entity, revision)
+    this.logger.verbose(
+      `Processed InteractReceiveCommand for receive ${receiveId}.`,
+      InteractReceiveCommandHandlerService.name,
+    )
   }
 }
