2cdef719578d87aa71422d7e600706e8edb862e4
Felix <jonfelixrico@gmail.com>
2021-07-10 19:04:27 +0800

    Implement logic in ReceiveWriteRepository

--- src/write-repositories/write-repositories.module.ts
+++ src/write-repositories/write-repositories.module.ts
@@ -3,10 +3,12 @@ import { PendingQuoteWriteRepositoryService } from './pending-quote-write-reposi
 import { EventStoreModule } from 'src/event-store/event-store.module'
 import { ReadStreamService } from './read-stream/read-stream.service'
 import { QuoteWriteRepositoryService } from './quote-write-repository/quote-write-repository.service'
+import { ReceiveWriteRepositoryService } from './receive-write-repository/receive-write-repository.service'
 
 const providersToExport = [
   PendingQuoteWriteRepositoryService,
   QuoteWriteRepositoryService,
+  ReceiveWriteRepositoryService,
 ]
 
 @Module({
