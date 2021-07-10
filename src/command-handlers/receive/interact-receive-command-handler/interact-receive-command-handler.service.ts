3b91a7579903b155b00bdcef64b32037f1f67011
Felix <jonfelixrico@gmail.com>
2021-07-10 18:23:42 +0800

    Add placeholder code to InteractRecieveCommandHandler

--- src/command-handlers/receive/interact-receive-command-handler/interact-receive-command-handler.service.ts
+++ src/command-handlers/receive/interact-receive-command-handler/interact-receive-command-handler.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common'
+import { EventStoreDBClient } from '@eventstore/db-client'
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { InteractReceiveCommand } from 'src/domain/commands/interact-receive.command'
 
-@Injectable()
-export class InteractReceiveCommandHandlerService {}
+@CommandHandler(InteractReceiveCommand)
+export class InteractReceiveCommandHandlerService
+  implements ICommandHandler<InteractReceiveCommand>
+{
+  async execute({ payload }: InteractReceiveCommand): Promise<any> {
+    const { karma, receiveId, userId } = payload
+    throw new Error('Method not implemented.')
+  }
+}
