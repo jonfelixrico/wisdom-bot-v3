bd24000e294ab5d45e00befa7b1cd4ca3b57362f
Felix <jonfelixrico@gmail.com>
2021-07-15 21:44:52 +0800

    Define imports in the ServicesModule

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
+import { CqrsModule } from '@nestjs/cqrs'
+import { ReadRepositoriesModule } from 'src/read-repositories/read-repositories.module'
 import { MessageRecacheService } from './message-recache/message-recache.service'
 import { MessageRegenerationService } from './message-regeneration/message-regeneration.service'
 
 @Module({
+  imports: [ReadRepositoriesModule, CqrsModule],
   providers: [MessageRecacheService, MessageRegenerationService],
 })
 export class ServicesModule {}
