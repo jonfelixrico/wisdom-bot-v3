c95ae58f395adbca8763661bd372d877f6302637
Felix <jonfelixrico@gmail.com>
2021-07-15 21:47:04 +0800

    Include DiscordModule as an import of ServicesModule

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { CqrsModule } from '@nestjs/cqrs'
+import { DiscordModule } from 'src/discord/discord.module'
 import { ReadRepositoriesModule } from 'src/read-repositories/read-repositories.module'
 import { MessageRecacheService } from './message-recache/message-recache.service'
 import { MessageRegenerationService } from './message-regeneration/message-regeneration.service'
 
 @Module({
-  imports: [ReadRepositoriesModule, CqrsModule],
+  imports: [ReadRepositoriesModule, CqrsModule, DiscordModule],
   providers: [MessageRecacheService, MessageRegenerationService],
 })
 export class ServicesModule {}
