a4ad9614215748cf967be879b0f2b81411c12cbc
Felix <jonfelixrico@gmail.com>
2021-07-17 01:18:43 +0800

    Remove ReactionListenerService

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -3,14 +3,12 @@ import { CqrsModule } from '@nestjs/cqrs'
 import { DiscordModule } from 'src/discord/discord.module'
 import { RegeneratePendingQuoteMessageCommandHandlerService } from './command-handlers/regenerate-pending-quote-message-command-handler/regenerate-pending-quote-message-command-handler.service'
 import { WatchPendingQuoteCommandHandlerService } from './command-handlers/watch-pending-quote-command-handler/watch-pending-quote-command-handler.service'
-import { ReactionListenerService } from './services/reaction-listener/reaction-listener.service'
 
 @Module({
   imports: [CqrsModule, DiscordModule],
   providers: [
     RegeneratePendingQuoteMessageCommandHandlerService,
     WatchPendingQuoteCommandHandlerService,
-    ReactionListenerService,
   ],
 })
 export class InfrastructureModule {}
