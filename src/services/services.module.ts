3afa0524204eb8f854b73ee0f299c7114c63d147
Felix <jonfelixrico@gmail.com>
2021-07-16 22:05:49 +0800

    Remove the old MessageRegenerationService

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -3,10 +3,9 @@ import { CqrsModule } from '@nestjs/cqrs'
 import { DiscordModule } from 'src/discord/discord.module'
 import { ReadRepositoriesModule } from 'src/read-repositories/read-repositories.module'
 import { MessageRecacheService } from './message-recache/message-recache.service'
-import { MessageRegenerationService } from './message-regeneration/message-regeneration.service'
 
 @Module({
   imports: [ReadRepositoriesModule, CqrsModule, DiscordModule],
-  providers: [MessageRecacheService, MessageRegenerationService],
+  providers: [MessageRecacheService],
 })
 export class ServicesModule {}
