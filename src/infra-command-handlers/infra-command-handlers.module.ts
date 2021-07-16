06ff7f61007d23d46a99627d04045d0bf92b33b1
Felix <jonfelixrico@gmail.com>
2021-07-16 23:06:29 +0800

    Generate boilerplate code for WatchPendingQuoteCommandHandler

--- src/infra-command-handlers/infra-command-handlers.module.ts
+++ src/infra-command-handlers/infra-command-handlers.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common'
 import { CqrsModule } from '@nestjs/cqrs'
 import { DiscordModule } from 'src/discord/discord.module'
 import { RegeneratePendingQuoteMessageCommandHandlerService } from './regenerate-pending-quote-message-command-handler/regenerate-pending-quote-message-command-handler.service'
+import { WatchPendingQuoteCommandHandlerService } from './watch-pending-quote-command-handler/watch-pending-quote-command-handler.service'
 
 @Module({
   imports: [CqrsModule, DiscordModule],
-  providers: [RegeneratePendingQuoteMessageCommandHandlerService],
+  providers: [
+    RegeneratePendingQuoteMessageCommandHandlerService,
+    WatchPendingQuoteCommandHandlerService,
+  ],
 })
 export class InfraCommandHandlersModule {}
---
+++ src/infra-command-handlers/watch-pending-quote-command-handler/watch-pending-quote-command-handler.service.ts
@@ -0,0 +1,11 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+import { WatchPendingQuoteCommand } from '../commands/watch-pending-quote.command'
+
+@CommandHandler(WatchPendingQuoteCommand)
+export class WatchPendingQuoteCommandHandlerService
+  implements ICommandHandler<WatchPendingQuoteCommand>
+{
+  execute(command: WatchPendingQuoteCommand): Promise<any> {
+    throw new Error('Method not implemented.')
+  }
+}
