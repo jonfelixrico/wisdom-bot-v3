5f24d05663185dfe09240bc5376bca72f80d432d
Felix <jonfelixrico@gmail.com>
2021-07-17 01:47:46 +0800

    Add reducer for QUOTE_MESSAGE_ID_UPDATED

--- src/write-repositories/reducers/pending-quote.reducer.ts
+++ src/write-repositories/reducers/pending-quote.reducer.ts
@@ -2,6 +2,7 @@ import { DomainEventNames } from 'src/domain/domain-event-names.enum'
 import { IPendingQuote } from 'src/domain/entities/pending-quote.interface'
 import { IPendingQuoteAcceptedPayload } from 'src/domain/events/pending-quote-accepted.event'
 import { IPendingQuoteCancelledPayload } from 'src/domain/events/pending-quote-cancelled.event'
+import { IQuoteMessageIdUpdatedPayload } from 'src/domain/events/quote-message-id-updated.event'
 import {
   WriteRepositoryReducer,
   WriteRepositoryReducerMap,
@@ -25,10 +26,23 @@ const quoteAccepted: WriteRepositoryReducer = (
   return state
 }
 
-const { PENDING_QUOTE_ACCEPTED, PENDING_QUOTE_CANCELLED, QUOTE_SUBMITTED } =
-  DomainEventNames
+function messageIdUpdated(
+  state: IPendingQuote,
+  { messageId }: IQuoteMessageIdUpdatedPayload,
+) {
+  state.messageId = messageId
+  return state
+}
+
+const {
+  PENDING_QUOTE_ACCEPTED,
+  PENDING_QUOTE_CANCELLED,
+  QUOTE_SUBMITTED,
+  QUOTE_MESSAGE_ID_UPDATED,
+} = DomainEventNames
 export const PENDING_QUOTE_REDUCERS: WriteRepositoryReducerMap = {
   [PENDING_QUOTE_ACCEPTED]: quoteAccepted,
   [PENDING_QUOTE_CANCELLED]: quoteCancelled,
   [QUOTE_SUBMITTED]: quoteSubmitted,
+  [QUOTE_MESSAGE_ID_UPDATED]: messageIdUpdated,
 }
