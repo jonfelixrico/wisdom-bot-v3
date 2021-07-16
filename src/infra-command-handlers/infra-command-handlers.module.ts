c99c2a98ddc1e61b3d482bbd8e9017d1638ff698
Felix <jonfelixrico@gmail.com>
2021-07-16 22:51:06 +0800

    Include necessary imports in InfraCommandHandlersModule

--- src/infra-command-handlers/infra-command-handlers.module.ts
+++ src/infra-command-handlers/infra-command-handlers.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common'
+import { CqrsModule } from '@nestjs/cqrs'
+import { DiscordModule } from 'src/discord/discord.module'
 import { RegeneratePendingQuoteMessageCommandHandlerService } from './regenerate-pending-quote-message-command-handler/regenerate-pending-quote-message-command-handler.service'
 
 @Module({
+  imports: [CqrsModule, DiscordModule],
   providers: [RegeneratePendingQuoteMessageCommandHandlerService],
 })
 export class InfraCommandHandlersModule {}
