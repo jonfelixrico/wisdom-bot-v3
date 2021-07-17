1a2b8d47be882b83d3cfa98a550bef659252d1ee
Felix <jonfelixrico@gmail.com>
2021-07-18 00:09:33 +0800

    Group handlers under the commando module under a common folder

--- src/commando/commando.module.ts
+++ src/commando/commando.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common'
-import { SubmitCommandService } from './submit-command/submit-command.service'
+import { SubmitCommandService } from './handlers/submit-command/submit-command.service'
 import { DiscordModule } from 'src/discord/discord.module'
 import { SetupService } from './setup/setup.service'
 import { CqrsModule } from '@nestjs/cqrs'
 import { TypeormModule } from 'src/typeorm/typeorm.module'
-import { ReceiveCommandService } from './receive-command/receive-command.service'
+import { ReceiveCommandService } from './handlers/receive-command/receive-command.service'
 import { ReadRepositoriesModule } from 'src/read-repositories/read-repositories.module'
-import { ConcurCommandService } from './concur-command/concur-command.service'
+import { ConcurCommandService } from './handlers/concur-command/concur-command.service'
 
 @Module({
   imports: [DiscordModule, CqrsModule, TypeormModule, ReadRepositoriesModule],
