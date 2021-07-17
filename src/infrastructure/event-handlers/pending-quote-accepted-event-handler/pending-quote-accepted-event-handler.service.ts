b2017fc227a547db964149d9a27fe4cf9ea00ea9
Felix <jonfelixrico@gmail.com>
2021-07-17 21:12:47 +0800

    Create event handler for the PendingQuoteAcceptedEvent

--- src/infrastructure/event-handlers/pending-quote-accepted-event-handler/pending-quote-accepted-event-handler.service.ts
+++ src/infrastructure/event-handlers/pending-quote-accepted-event-handler/pending-quote-accepted-event-handler.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common'
+import { Logger } from '@nestjs/common'
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
+import { MessageEmbed } from 'discord.js'
+import { DiscordHelperService } from 'src/discord/discord-helper/discord-helper.service'
+import { PendingQuoteAcceptedEvent } from 'src/infrastructure/events/pending-quote-accepted.event'
+import { QuoteQueryService } from 'src/read-repositories/queries/quote-query/quote-query.service'
 
-@Injectable()
-export class PendingQuoteAcceptedEventHandlerService {}
+const EMPTY_STRING = '\u200B'
+
+@EventsHandler(PendingQuoteAcceptedEvent)
+export class PendingQuoteAcceptedEventHandlerService
+  implements IEventHandler<PendingQuoteAcceptedEvent>
+{
+  constructor(
+    private query: QuoteQueryService,
+    private helper: DiscordHelperService,
+    private logger: Logger,
+  ) {}
+
+  async handle({ payload }: PendingQuoteAcceptedEvent) {
+    const { quoteId } = payload
+    const quote = await this.query.getQuoteData(quoteId)
+
+    if (!quote) {
+      this.logger.warn(
+        `Quote ${quoteId} does not exist.`,
+        PendingQuoteAcceptedEventHandlerService.name,
+      )
+      return
+    }
+
+    const { helper } = this
+    const {
+      guildId,
+      channelId,
+      messageId,
+      content,
+      submitterId,
+      authorId,
+      year,
+    } = quote
+
+    const channel = await helper.getTextChannel(guildId, channelId)
+
+    if (!channel) {
+      this.logger.warn(
+        `Channel ${channelId} does not exist.`,
+        PendingQuoteAcceptedEventHandlerService.name,
+      )
+      return
+    }
+
+    await helper.deleteMessage(guildId, channelId, messageId)
+    // TODO add logging that indicates if deleteMessage was successful
+
+    const embeddedMessage = new MessageEmbed({
+      title: 'Quote accepted!',
+      description: `${content} - <@${authorId}>, ${year}`,
+      fields: [
+        {
+          name: EMPTY_STRING,
+          value: `Submitted by <@${submitterId}>`,
+        },
+      ],
+    })
+
+    return await channel.send(embeddedMessage)
+  }
+}
--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common'
 import { CqrsModule } from '@nestjs/cqrs'
 import { DiscordModule } from 'src/discord/discord.module'
+import { ReadRepositoriesModule } from 'src/read-repositories/read-repositories.module'
 import { RegeneratePendingQuoteMessageCommandHandlerService } from './command-handlers/regenerate-pending-quote-message-command-handler/regenerate-pending-quote-message-command-handler.service'
 import { WatchPendingQuoteCommandHandlerService } from './command-handlers/watch-pending-quote-command-handler/watch-pending-quote-command-handler.service'
 import { PendingQuoteAcceptedEventHandlerService } from './event-handlers/pending-quote-accepted-event-handler/pending-quote-accepted-event-handler.service'
 import { PendingQuoteExpiredEventHandlerService } from './event-handlers/pending-quote-expired-event-handler/pending-quote-expired-event-handler.service'
 
 @Module({
-  imports: [CqrsModule, DiscordModule],
+  imports: [CqrsModule, DiscordModule, ReadRepositoriesModule],
   providers: [
     RegeneratePendingQuoteMessageCommandHandlerService,
     WatchPendingQuoteCommandHandlerService,
