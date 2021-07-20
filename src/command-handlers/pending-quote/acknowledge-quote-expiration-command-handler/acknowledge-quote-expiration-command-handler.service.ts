747f7ba88d177841066726678ef04f473e8d9367
Felix <jonfelixrico@gmail.com>
2021-07-20 16:39:45 +0800

    Implement the functionality of AcknowledgeQuoteExpirationCommandHandler

--- src/command-handlers/pending-quote/acknowledge-quote-expiration-command-handler/acknowledge-quote-expiration-command-handler.service.ts
+++ src/command-handlers/pending-quote/acknowledge-quote-expiration-command-handler/acknowledge-quote-expiration-command-handler.service.ts
@@ -1,11 +1,39 @@
+import { Logger } from '@nestjs/common'
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
 import { AcknowledgePendingQuoteExpirationCommand } from 'src/domain/commands/acknowledge-pending-quote-expiration.command'
+import { PendingQuoteWriteRepositoryService } from 'src/write-repositories/pending-quote-write-repository/pending-quote-write-repository.service'
 
 @CommandHandler(AcknowledgePendingQuoteExpirationCommand)
 export class AcknowledgeQuoteExpirationCommandHandlerService
   implements ICommandHandler<AcknowledgePendingQuoteExpirationCommand>
 {
-  execute(command: AcknowledgePendingQuoteExpirationCommand): Promise<any> {
-    throw new Error('Method not implemented.')
+  constructor(
+    private repo: PendingQuoteWriteRepositoryService,
+    private logger: Logger,
+  ) {}
+
+  async execute({
+    payload,
+  }: AcknowledgePendingQuoteExpirationCommand): Promise<any> {
+    const { quoteId } = payload
+
+    const result = await this.repo.findById(quoteId)
+
+    if (!result) {
+      this.logger.warn(
+        `Quote ${quoteId} not found.`,
+        AcknowledgeQuoteExpirationCommandHandlerService.name,
+      )
+    }
+
+    const { entity, revision } = result
+
+    entity.acknowledgeExpiration()
+    await this.repo.publishEvents(entity, revision)
+
+    this.logger.verbose(
+      `Flagged quote ${quoteId} as expired.`,
+      AcknowledgePendingQuoteExpirationCommand.name,
+    )
   }
 }
