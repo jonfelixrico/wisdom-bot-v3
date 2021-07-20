1438dbb4c677202b9a00de8b2c0ed706cede6788
Felix <jonfelixrico@gmail.com>
2021-07-20 21:42:50 +0800

    Implement query service for guild-wide stats

--- src/read-repositories/queries/guild-stats-query/guild-stats-query.service.ts
+++ src/read-repositories/queries/guild-stats-query/guild-stats-query.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common'
+import { InteractionTypeormRepository } from 'src/typeorm/providers/interaction.typeorm-repository'
+import { QuoteTypeormRepository } from 'src/typeorm/providers/quote.typeorm-repository'
+import { ReceiveTypeormRepository } from 'src/typeorm/providers/receive.typeorm-repository'
 
 @Injectable()
-export class GuildStatsQueryService {}
+export class GuildStatsQueryService {
+  constructor(
+    private quoteRepo: QuoteTypeormRepository,
+    private receiveRepo: ReceiveTypeormRepository,
+    private interactionRepo: InteractionTypeormRepository,
+  ) {}
+
+  async getStats(guildId: string) {
+    const quotes = await this.quoteRepo
+      .createQueryBuilder()
+      .where('guildId = :guildId', { guildId })
+      .andWhere('acceptDt IS NOT NULL')
+      .getCount()
+
+    const receives = await this.receiveRepo
+      .createQueryBuilder()
+      .where('guildId = :guildId', { guildId })
+      .getCount()
+
+    const interactions = await this.interactionRepo
+      .createQueryBuilder()
+      .where('guildId = :guildId', { guildId })
+      .getCount()
+
+    return {
+      quotes,
+      receives,
+      interactions,
+    }
+  }
+}
