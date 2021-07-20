daee0ab38a406e77d4b1b067dbd2d0971562a44e
Felix <jonfelixrico@gmail.com>
2021-07-20 21:24:32 +0800

    Create service for user stats

--- src/read-repositories/queries/user-stats-query/user-stats-query.service.ts
+++ src/read-repositories/queries/user-stats-query/user-stats-query.service.ts
@@ -1,4 +1,72 @@
 import { Injectable } from '@nestjs/common'
+import { InteractionTypeormRepository } from 'src/typeorm/providers/interaction.typeorm-repository'
+import { QuoteTypeormRepository } from 'src/typeorm/providers/quote.typeorm-repository'
+import { ReceiveTypeormRepository } from 'src/typeorm/providers/receive.typeorm-repository'
 
 @Injectable()
-export class UserStatsQueryService {}
+export class UserStatsQueryService {
+  constructor(
+    private quoteRepo: QuoteTypeormRepository,
+    private receiveRepo: ReceiveTypeormRepository,
+    private interactionRepo: InteractionTypeormRepository,
+  ) {}
+
+  private async outgoingStats(guildId: string, userId: string) {
+    const submittedQuotesCount = await this.quoteRepo
+      .createQueryBuilder()
+      .where('guildId = :guildId', { guildId })
+      .andWhere('submitterId = :userId', { userId })
+      .getCount()
+
+    const receivedQuoteCount = await this.receiveRepo
+      .createQueryBuilder()
+      .where('guildId = :guildId', { guildId })
+      .andWhere('userId = :userId', { userId })
+      .getCount()
+
+    const interactionQuoteCount = await this.interactionRepo
+      .createQueryBuilder()
+      .where('guildId = :guildId', { guildId })
+      .andWhere('userId = :userId', { userId })
+      .getCount()
+
+    return {
+      receivesGiven: receivedQuoteCount,
+      interactionsGiven: interactionQuoteCount,
+      quotesSubmitted: submittedQuotesCount,
+    }
+  }
+
+  private async incomingStats(guildId: string, userId: string) {
+    const authoredQuotesCount = await this.quoteRepo
+      .createQueryBuilder()
+      .where('guildId = :guildId', { guildId })
+      .andWhere('authorId = :userId', { userId })
+      .getCount()
+
+    const quoteReceivesCount = await this.receiveRepo
+      .createQueryBuilder()
+      .where('guildId = :guildId', { guildId })
+      .andWhere('parentQuoteAuthorId = :userId', { userId })
+      .getCount()
+
+    const quoteInteractionsCount = await this.interactionRepo
+      .createQueryBuilder()
+      .where('guildId = :guildId', { guildId })
+      .andWhere('parentQuoteAuthorId = :userId', { userId })
+      .getCount()
+
+    return {
+      receivesReceived: quoteReceivesCount,
+      quotesAuthored: authoredQuotesCount,
+      interactionsReceived: quoteInteractionsCount,
+    }
+  }
+
+  async getStats(guildId: string, userId: string) {
+    return {
+      ...(await this.incomingStats(guildId, userId)),
+      ...(await this.outgoingStats(guildId, userId)),
+    }
+  }
+}
