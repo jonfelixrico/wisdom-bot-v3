c264877e816dedc7f959df9ade5f58b3177f5605
Felix <jonfelixrico@gmail.com>
2021-07-21 23:53:08 +0800

    Create event for notifying services that a new event has been published by the write repositories

---
+++ src/write-repositories/events/domain-event-published.event.ts
@@ -0,0 +1,7 @@
+import { ReadRevision } from '@eventstore/db-client'
+import { IEvent } from '@nestjs/cqrs'
+import { DomainEvent } from 'src/domain/abstracts/domain-event.abstract'
+
+export class DomainEventPublishedEvent implements IEvent {
+  constructor(readonly event: DomainEvent, readonly revision: ReadRevision) {}
+}
