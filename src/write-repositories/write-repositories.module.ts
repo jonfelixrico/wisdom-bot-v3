5a119e1d9ee43a6c0864cb84161533e910288ba2
Felix <jonfelixrico@gmail.com>
2021-07-22 00:21:45 +0800

    Remove unused services from the WriteRepositoriesModule

--- src/write-repositories/write-repositories.module.ts
+++ src/write-repositories/write-repositories.module.ts
@@ -1,7 +1,6 @@
 import { Module, Provider } from '@nestjs/common'
 import { PendingQuoteWriteRepositoryService } from './pending-quote-write-repository/pending-quote-write-repository.service'
 import { EventStoreModule } from 'src/event-store/event-store.module'
-import { ReadStreamService } from './read-stream/read-stream.service'
 import { QuoteWriteRepositoryService } from './quote-write-repository/quote-write-repository.service'
 import { ReceiveWriteRepositoryService } from './receive-write-repository/receive-write-repository.service'
 import { PendingQuoteWriteRepository } from './abstract/pending-quote-write-repository.abstract'
@@ -24,7 +23,7 @@ const providersToExport: Provider[] = [
 ]
 
 @Module({
-  providers: [...providersToExport, ReadStreamService],
+  providers: [...providersToExport],
   imports: [EventStoreModule],
   exports: providersToExport,
 })
