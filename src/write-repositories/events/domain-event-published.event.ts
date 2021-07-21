adcaa5cd9e4fc9c71b056c3045735326575418b3
Felix <jonfelixrico@gmail.com>
2021-07-22 00:03:51 +0800

    Use bigint in DomainEventPublishedEvent#revision

--- src/write-repositories/events/domain-event-published.event.ts
+++ src/write-repositories/events/domain-event-published.event.ts
@@ -1,7 +1,6 @@
-import { ReadRevision } from '@eventstore/db-client'
 import { IEvent } from '@nestjs/cqrs'
 import { DomainEvent } from 'src/domain/abstracts/domain-event.abstract'
 
 export class DomainEventPublishedEvent implements IEvent {
-  constructor(readonly event: DomainEvent, readonly revision: ReadRevision) {}
+  constructor(readonly event: DomainEvent, readonly revision: bigint) {}
 }
