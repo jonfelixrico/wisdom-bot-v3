b1cbbdd93ed842ccddc0e67f06ccb56bb0e047c5
Felix <jonfelixrico@gmail.com>
2021-07-21 22:38:18 +0800

    Revert "Move the `publishEvents` method to a separate interface"
    This reverts commit b0649198851d3561f0d5e0fde40f85c1c4df8447.

--- src/write-repositories/abstract/esdb-repository.abstract.ts
+++ src/write-repositories/abstract/esdb-repository.abstract.ts
@@ -1,19 +1,16 @@
 import { ExpectedRevision } from '@eventstore/db-client'
 import { DomainEntity } from 'src/domain/abstracts/domain-entity.abstract'
-import { IEventPublishingRepository } from './event-publishing-repository.interface'
 
 export interface IEsdbRepositoryEntity<Entity> {
   entity: Entity
   revision: bigint
 }
 
-export abstract class EsdbRepository<Entity extends DomainEntity>
-  implements IEventPublishingRepository<Entity>
-{
+export abstract class EsdbRepository<Entity extends DomainEntity> {
+  abstract findById(id: string): Promise<IEsdbRepositoryEntity<Entity>>
+
   abstract publishEvents(
     entity: Entity,
     expectedRevision: ExpectedRevision,
   ): Promise<void>
-
-  abstract findById(id: string): Promise<IEsdbRepositoryEntity<Entity>>
 }
--- src/write-repositories/abstract/event-publishing-repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { ExpectedRevision } from '@eventstore/db-client'
-import { DomainEntity } from 'src/domain/abstracts/domain-entity.abstract'
-
-export interface IEventPublishingRepository<Entity extends DomainEntity> {
-  publishEvents(
-    entity: Entity,
-    expectedRevision: ExpectedRevision,
-  ): Promise<void>
-}
