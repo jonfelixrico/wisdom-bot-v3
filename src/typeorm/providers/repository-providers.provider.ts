02e614c41294faa2daab215c3b007db8cf466d14
Felix <jonfelixrico@gmail.com>
2021-07-22 22:04:38 +0800

    Register the guild typeorm entity; apply minor changes to the way repository providers are exported

--- src/typeorm/providers/repository-providers.provider.ts
+++ src/typeorm/providers/repository-providers.provider.ts
@@ -3,23 +3,27 @@ import { QuoteTypeormEntity } from '../entities/quote.typeorm-entity'
 import { ReceiveTypeormEntity } from '../entities/receive.typeorm-entity'
 import { QuoteTypeormRepository } from './quote.typeorm-repository'
 import { ReceiveTypeormRepository } from './receive.typeorm-repository'
-import { generateTypeormRepositoryProvider } from './typeorm-provider-factory.util'
 import { ReactionTypeormEntity } from '../entities/reaction.typeorm-entity'
 import { ReactionTypeormRepository } from './reaction.typeorm-repository'
+import { Connection, Repository } from 'typeorm'
+import { GuildTypeormEntity } from '../entities/guild.typeorm-entity'
+import { GuildTypeormRepository } from './guild.typeorm-repository'
 
-export const typeormRepositoryProviders: Provider[] = [
-  generateTypeormRepositoryProvider<QuoteTypeormEntity, QuoteTypeormRepository>(
-    QuoteTypeormEntity,
-    QuoteTypeormRepository,
-  ),
+type EntityPair<Entity> = [new () => Entity, new () => Repository<Entity>]
 
-  generateTypeormRepositoryProvider(
-    ReceiveTypeormEntity,
-    ReceiveTypeormRepository,
-  ),
-
-  generateTypeormRepositoryProvider(
-    ReactionTypeormEntity,
-    ReactionTypeormRepository,
-  ),
+const entityPairs: EntityPair<unknown>[] = [
+  [QuoteTypeormEntity, QuoteTypeormRepository],
+  [ReceiveTypeormEntity, ReceiveTypeormRepository],
+  [ReactionTypeormEntity, ReactionTypeormRepository],
+  [GuildTypeormEntity, GuildTypeormRepository],
 ]
+
+export const typeormRepositoryProviders: Provider[] = entityPairs.map(
+  ([entity, repo]) => {
+    return {
+      useFactory: (conn: Connection) => conn.getRepository(entity),
+      provide: repo,
+      inject: [Connection],
+    }
+  },
+)
