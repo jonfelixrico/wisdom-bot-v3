84016d2a74142f69ab6f1c9ec909b4919dbd905a
Felix <jonfelixrico@gmail.com>
2021-07-22 21:38:54 +0800

    Implement the guild write repository

--- src/write-repositories/guild-write-repository/guild-write-repository.service.ts
+++ src/write-repositories/guild-write-repository/guild-write-repository.service.ts
@@ -1,4 +1,40 @@
+import { ExpectedRevision } from '@eventstore/db-client'
 import { Injectable } from '@nestjs/common'
+import { Guild } from 'src/domain/entities/guild.entity'
+import { GuildWriteRepository } from '../abstract/guild-write-repository.abstract'
+import { DomainEventPublisherService } from '../domain-event-publisher/domain-event-publisher.service'
+import { EsdbHelperService } from '../esdb-helper/esdb-helper.service'
+import { GUILD_REDUCERS } from '../reducers/guild.reducer'
+import { reduceEvents } from '../reducers/reducer.util'
 
 @Injectable()
-export class GuildWriteRepositoryService {}
+export class GuildWriteRepositoryService extends GuildWriteRepository {
+  constructor(
+    private helper: EsdbHelperService,
+    private publisher: DomainEventPublisherService,
+  ) {
+    super()
+  }
+
+  async findById(id: string) {
+    const events = await this.helper.readAllEvents(id)
+
+    if (!events) {
+      return null
+    }
+
+    const [entity, revision] = reduceEvents(events, GUILD_REDUCERS)
+
+    return {
+      entity: new Guild(entity),
+      revision,
+    }
+  }
+
+  async publishEvents(
+    entity: Guild,
+    expectedRevision: ExpectedRevision,
+  ): Promise<void> {
+    await this.publisher.publishEvents(entity.events, expectedRevision)
+  }
+}
