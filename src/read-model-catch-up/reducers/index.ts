14f63f4170e0b3d98c0dc82927c0a64584616806
Felix <jonfelixrico@gmail.com>
2021-07-23 00:03:50 +0800

    Include the GUILD_REDUCERS map into the index file for read reducers

--- src/read-model-catch-up/reducers/index.ts
+++ src/read-model-catch-up/reducers/index.ts
@@ -1,8 +1,10 @@
 import { ReducerMap } from '../types/reducer-map.type'
 import { QUOTE_REDUCERS } from './quote.reducers'
 import { RECEIVE_REDUCERS } from './receive.reducers'
+import { GUILD_REDUCERS } from './guild.reducers'
 
 export const REDUCER_MAP: ReducerMap = {
   ...QUOTE_REDUCERS,
   ...RECEIVE_REDUCERS,
+  ...GUILD_REDUCERS,
 }
