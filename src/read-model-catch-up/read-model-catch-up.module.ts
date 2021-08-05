e4361c3bf4a4db0aedb200eefa26c2d01f4138a9
Felix <jonfelixrico@gmail.com>
2021-08-05 21:28:55 +0800

    Remove unused services under ReadModelCatchUpModule

--- src/read-model-catch-up/read-model-catch-up.module.ts
+++ src/read-model-catch-up/read-model-catch-up.module.ts
@@ -1,25 +1,12 @@
 import { Module } from '@nestjs/common'
-import { QuoteCatchUpService } from './catch-up/quote-catch-up/quote-catch-up.service'
-import { CatchUpOrchestratorService } from './catch-up/catch-up-orchestrator/catch-up-orchestrator.service'
 import { TypeormModule } from 'src/typeorm/typeorm.module'
 import { EventStoreModule } from 'src/event-store/event-store.module'
-import { ReceiveCatchUpService } from './catch-up/receive-catch-up/receive-catch-up.service'
-import { EventRelayService } from './services/event-relay/event-relay.service'
-import { EventListenerService } from './services/event-listener/event-listener.service'
 import { CqrsModule } from '@nestjs/cqrs'
 import { EventSyncNotifierService } from './services/event-sync-notifier/event-sync-notifier.service'
 import { CatchUpService } from './services/catch-up/catch-up.service'
 
 @Module({
-  providers: [
-    // QuoteCatchUpService,
-    // CatchUpOrchestratorService,
-    // ReceiveCatchUpService,
-    // EventRelayService,
-    // EventListenerService,
-    EventSyncNotifierService,
-    CatchUpService,
-  ],
+  providers: [EventSyncNotifierService, CatchUpService],
   imports: [TypeormModule, EventStoreModule, CqrsModule],
 })
 export class ReadModelCatchUpModule {}
