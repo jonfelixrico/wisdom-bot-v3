583163337b1f21ad201b9d2a7e4a2d066247b3f3
Felix <jonfelixrico@gmail.com>
2021-08-07 22:22:56 +0800

    Remove unnecessary use of queryBuilders with EntityManager#count

--- src/read-model-query/user-stats-query/user-stats-query.service.ts
+++ src/read-model-query/user-stats-query/user-stats-query.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { ReactionTypeormRepository } from 'src/typeorm/providers/reaction.typeorm-repository'
 import { QuoteTypeormRepository } from 'src/typeorm/providers/quote.typeorm-repository'
 import { ReceiveTypeormRepository } from 'src/typeorm/providers/receive.typeorm-repository'
+import { Not } from 'typeorm'
 
 @Injectable()
 export class UserStatsQueryService {
@@ -12,24 +13,27 @@ export class UserStatsQueryService {
   ) {}
 
   private async outgoingStats(guildId: string, userId: string) {
-    const submittedQuotesCount = await this.quoteRepo
-      .createQueryBuilder()
-      .where('guildId = :guildId', { guildId })
-      .andWhere('submitterId = :userId', { userId })
-      .andWhere('acceptDt IS NOT NULL')
-      .getCount()
+    const submittedQuotesCount = await this.quoteRepo.count({
+      where: {
+        guildId,
+        submitterId: userId,
+        acceptDt: Not(null),
+      },
+    })
 
-    const receiveCount = await this.receiveRepo
-      .createQueryBuilder()
-      .where('guildId = :guildId', { guildId })
-      .andWhere('userId = :userId', { userId })
-      .getCount()
+    const receiveCount = await this.receiveRepo.count({
+      where: {
+        guildId,
+        userId,
+      },
+    })
 
-    const reactionCount = await this.reactionRepo
-      .createQueryBuilder()
-      .where('guildId = :guildId', { guildId })
-      .andWhere('userId = :userId', { userId })
-      .getCount()
+    const reactionCount = await this.reactionRepo.count({
+      where: {
+        guildId,
+        userId,
+      },
+    })
 
     return {
       receiveCount,
@@ -39,24 +43,27 @@ export class UserStatsQueryService {
   }
 
   private async incomingStats(guildId: string, userId: string) {
-    const authoredQuotes = await this.quoteRepo
-      .createQueryBuilder()
-      .where('guildId = :guildId', { guildId })
-      .andWhere('authorId = :userId', { userId })
-      .andWhere('acceptDt IS NOT NULL')
-      .getCount()
+    const authoredQuotes = await this.quoteRepo.count({
+      where: {
+        guildId,
+        authorId: userId,
+        acceptDt: Not(null),
+      },
+    })
 
-    const timesReceived = await this.receiveRepo
-      .createQueryBuilder()
-      .where('guildId = :guildId', { guildId })
-      .andWhere('parentQuoteAuthorId = :userId', { userId })
-      .getCount()
+    const timesReceived = await this.receiveRepo.count({
+      where: {
+        guildId,
+        parentQuoteAuthorId: userId,
+      },
+    })
 
-    const timesReactedTo = await this.reactionRepo
-      .createQueryBuilder()
-      .where('guildId = :guildId', { guildId })
-      .andWhere('parentQuoteAuthorId = :userId', { userId })
-      .getCount()
+    const timesReactedTo = await this.reactionRepo.count({
+      where: {
+        guildId,
+        parentQuoteAuthorId: userId,
+      },
+    })
 
     return {
       timesReceived,
