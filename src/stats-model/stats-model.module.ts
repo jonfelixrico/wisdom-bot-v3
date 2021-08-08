5a731823ccafcf793507bbcc21eb17040d470ba1
Felix <jonfelixrico@gmail.com>
2021-08-08 20:54:19 +0800

    Revert "Provide EntityManager"
    This reverts commit 21b3c017a123464840f311e63743162bcf11b030.

--- src/stats-model/stats-model.module.ts
+++ src/stats-model/stats-model.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { EventStoreModule } from 'src/event-store/event-store.module'
-import { statsModelTypeormProviders } from './typeorm/stats-model-typeorm.providers'
+import { statsModelTypeormProvider } from './typeorm/stats-model-typeorm.provider'
 import { StatsModelCatchUpService } from './services/catch-up/stats-model-catch-up.service'
 import { GuildTopContributorsQueryHandlerService } from './query-handlers/guild-top-contributors-query-handler/guild-top-contributors-query-handler.service'
 import { AuthorTopContributorsQueryHandlerService } from './query-handlers/author-top-contributors-query-handler/author-top-contributors-query-handler.service'
@@ -9,7 +9,7 @@ import { CqrsModule } from '@nestjs/cqrs'
 @Module({
   imports: [EventStoreModule, CqrsModule],
   providers: [
-    ...statsModelTypeormProviders,
+    statsModelTypeormProvider,
     StatsModelCatchUpService,
     GuildTopContributorsQueryHandlerService,
     AuthorTopContributorsQueryHandlerService,
---
+++ src/stats-model/typeorm/stats-model-typeorm.provider.ts
@@ -0,0 +1,20 @@
+import { Provider } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { MapTypeormEntity } from 'src/typeorm/entities/map.typeorm-entity'
+import { Connection, createConnection } from 'typeorm'
+
+export const statsModelTypeormProvider: Provider = {
+  provide: Connection,
+  useFactory: async (cfg: ConfigService) =>
+    await createConnection({
+      type: 'postgres',
+      url: cfg.get('STATS_DB_URL'),
+      synchronize: !!cfg.get('STATS_DB_SYNC'),
+      entities: [
+        __dirname + '/entities/*.typeorm-entity{.ts,.js}',
+        MapTypeormEntity,
+      ],
+      name: 'stats-model',
+    }),
+  inject: [ConfigService],
+}
