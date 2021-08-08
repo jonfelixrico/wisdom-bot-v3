a5c2f534dd55cd697415e21ead5099200bb86197
Felix <jonfelixrico@gmail.com>
2021-08-08 21:41:12 +0800

    Allow overriding of the command prefix

--- src/commando/services/setup/setup.service.ts
+++ src/commando/services/setup/setup.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { CommandoClient } from 'discord.js-commando'
 
 @Injectable()
 export class SetupService {
-  constructor(client: CommandoClient) {
-    client.commandPrefix = '~wisdom'
+  constructor(client: CommandoClient, config: ConfigService) {
+    // default prefix
+    client.commandPrefix = config.get('DISCORD_PREFIX') || `~wisdom`
+
     client.registry.registerDefaultTypes()
     client.registry.registerDefaultGroups()
     client.registry.registerDefaultCommands({
