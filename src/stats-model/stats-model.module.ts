21b3c017a123464840f311e63743162bcf11b030
Felix <jonfelixrico@gmail.com>
2021-08-08 20:54:19 +0800

    Provide EntityManager

--- src/stats-model/stats-model.module.ts
+++ src/stats-model/stats-model.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { EventStoreModule } from 'src/event-store/event-store.module'
-import { statsModelTypeormProvider } from './typeorm/stats-model-typeorm.provider'
+import { statsModelTypeormProviders } from './typeorm/stats-model-typeorm.providers'
 import { StatsModelCatchUpService } from './services/catch-up/stats-model-catch-up.service'
 import { GuildTopContributorsQueryHandlerService } from './query-handlers/guild-top-contributors-query-handler/guild-top-contributors-query-handler.service'
 import { AuthorTopContributorsQueryHandlerService } from './query-handlers/author-top-contributors-query-handler/author-top-contributors-query-handler.service'
@@ -9,7 +9,7 @@ import { CqrsModule } from '@nestjs/cqrs'
 @Module({
   imports: [EventStoreModule, CqrsModule],
   providers: [
-    statsModelTypeormProvider,
+    ...statsModelTypeormProviders,
     StatsModelCatchUpService,
     GuildTopContributorsQueryHandlerService,
     AuthorTopContributorsQueryHandlerService,
