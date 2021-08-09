e1566aa549e35845c15400866c81ce270995be4d
Felix <jonfelixrico@gmail.com>
2021-08-09 18:25:30 +0800

    Create query handler for the top receivers

--- src/stats-model/query-handlers/author-top-receivers-query-handler/author-top-receivers-query-handler.service.ts
+++ src/stats-model/query-handlers/author-top-receivers-query-handler/author-top-receivers-query-handler.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common'
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
+import {
+  AuthorTopReceiversQuery,
+  IAuthorTopReceiversQueryOutput,
+} from 'src/queries/author-top-receivers.query'
+import { GuildMemberInteractionTypeormEntity } from 'src/stats-model/typeorm/entities/guild-member-interaction.typeorm-entity'
+import { Connection, MoreThan } from 'typeorm'
 
-@Injectable()
-export class AuthorTopReceiversQueryHandlerService {}
+@QueryHandler(AuthorTopReceiversQuery)
+export class AuthorTopContributorsQueryHandlerService
+  implements IQueryHandler<AuthorTopReceiversQuery>
+{
+  constructor(private conn: Connection) {}
+
+  private get repo() {
+    return this.conn.getRepository(GuildMemberInteractionTypeormEntity)
+  }
+
+  async execute({
+    input,
+  }: AuthorTopReceiversQuery): Promise<IAuthorTopReceiversQueryOutput> {
+    const { guildId, limit, authorId } = input
+
+    const results = await this.repo.find({
+      where: {
+        guildId,
+        targetUserId: authorId,
+        receives: MoreThan(0),
+      },
+      take: limit,
+      order: {
+        // TODO create tiebreaker for same `receives` count
+        receives: 'DESC',
+      },
+    })
+
+    return results.map(({ userId, receives }) => {
+      return {
+        userId,
+        receives,
+      }
+    })
+  }
+}
