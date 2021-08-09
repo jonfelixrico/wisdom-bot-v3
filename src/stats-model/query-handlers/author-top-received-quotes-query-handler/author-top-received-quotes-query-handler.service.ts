a44a0ef4e455987fb4e0377dda54ba9983e51081
Felix <jonfelixrico@gmail.com>
2021-08-09 19:00:09 +0800

    Implement query handler for AuthorTopReceivedQuotesQuery

--- src/stats-model/query-handlers/author-top-received-quotes-query-handler/author-top-received-quotes-query-handler.service.ts
+++ src/stats-model/query-handlers/author-top-received-quotes-query-handler/author-top-received-quotes-query-handler.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common'
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
+import {
+  AuthorTopReceivedQuotesQuery,
+  IAuthorTopReceivedQuotesQueryOutput,
+} from 'src/queries/author-top-received-quotes.query'
+import { QuoteInfoTypeormEntity } from 'src/stats-model/typeorm/entities/quote-info.typeorm-entity'
+import { Connection, MoreThan } from 'typeorm'
 
-@Injectable()
-export class AuthorTopReceivedQuotesQueryHandlerService {}
+@QueryHandler(AuthorTopReceivedQuotesQuery)
+export class AuthorTopReceivedQuotesQueryHandlerService
+  implements IQueryHandler<AuthorTopReceivedQuotesQuery>
+{
+  constructor(private conn: Connection) {}
+
+  async execute({
+    input,
+  }: AuthorTopReceivedQuotesQuery): Promise<IAuthorTopReceivedQuotesQueryOutput> {
+    const { guildId, limit, authorId } = input
+
+    const results = await this.conn.getRepository(QuoteInfoTypeormEntity).find({
+      take: limit,
+      where: { guildId, receives: MoreThan(0), authorId },
+      order: {
+        receives: 'DESC',
+      },
+    })
+
+    return results.map(({ content, authorId, receives, quoteId }) => {
+      return {
+        content,
+        authorId,
+        receives,
+        quoteId,
+      }
+    })
+  }
+}
