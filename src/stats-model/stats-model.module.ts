a3e8d05ddc0ca15fe03d529df1602d0d8c7f7b44
Felix <jonfelixrico@gmail.com>
2021-08-09 18:10:41 +0800

    Revert "Generate placeholder code for query handlers"
    This reverts commit b04eb5e2d18a5bf2452ea1cfc9c251720f6ad88f.

--- src/stats-model/stats-model.module.ts
+++ src/stats-model/stats-model.module.ts
@@ -5,8 +5,6 @@ import { StatsModelCatchUpService } from './services/catch-up/stats-model-catch-
 import { GuildTopContributorsQueryHandlerService } from './query-handlers/guild-top-contributors-query-handler/guild-top-contributors-query-handler.service'
 import { AuthorTopContributorsQueryHandlerService } from './query-handlers/author-top-contributors-query-handler/author-top-contributors-query-handler.service'
 import { CqrsModule } from '@nestjs/cqrs'
-import { TopReceivedAuthorsQueryHandlerService } from './query-handlers/top-received-authors-query-handler/top-received-authors-query-handler.service'
-import { TopReceiversQueryHandlerService } from './query-handlers/top-receivers-query-handler/top-receivers-query-handler.service'
 
 @Module({
   imports: [EventStoreModule, CqrsModule],
@@ -15,8 +13,6 @@ import { TopReceiversQueryHandlerService } from './query-handlers/top-receivers-
     StatsModelCatchUpService,
     GuildTopContributorsQueryHandlerService,
     AuthorTopContributorsQueryHandlerService,
-    TopReceivedAuthorsQueryHandlerService,
-    TopReceiversQueryHandlerService,
   ],
 })
 export class StatsModelModule {}
