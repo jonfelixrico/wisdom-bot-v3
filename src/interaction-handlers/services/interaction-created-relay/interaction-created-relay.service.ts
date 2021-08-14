3240c6e98d0dca38cce9a451180170aa46fc52ce
Felix <jonfelixrico@gmail.com>
2021-08-14 22:55:07 +0800

    Implement logic in InteractionCreatedRelayService

--- src/interaction-handlers/services/interaction-created-relay/interaction-created-relay.service.ts
+++ src/interaction-handlers/services/interaction-created-relay/interaction-created-relay.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common'
+import { EventBus } from '@nestjs/cqrs'
+import { Client } from 'discord.js'
+import { DiscordInteractionEvent } from 'src/interaction-handlers/types/discord-interaction.event'
 
 @Injectable()
-export class InteractionCreatedRelayService {}
+export class InteractionCreatedRelayService implements OnApplicationBootstrap {
+  constructor(private client: Client, private eventBus: EventBus) {}
+
+  onApplicationBootstrap() {
+    const { client, eventBus } = this
+    client.on('interactionCreate', (interaction) => {
+      eventBus.publish(new DiscordInteractionEvent(interaction))
+    })
+  }
+}
