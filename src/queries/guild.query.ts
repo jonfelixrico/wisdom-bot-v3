f65efc8ace2ec838b2971dfca8fcbdea6deb421c
Felix <jonfelixrico@gmail.com>
2021-08-15 01:47:22 +0800

    Add comments to GuildQuery

--- src/queries/guild.query.ts
+++ src/queries/guild.query.ts
@@ -1,19 +1,24 @@
 import { IQuery } from '@nestjs/cqrs'
 import { IQuoteSettings, ISettings } from 'src/domain/entities/guild.interfaces'
 
+export interface IGuildQueryInput {
+  guildId: string
+
+  // tells the query handler to return the guild default values instead if the guild is not found
+  existingOnly?: boolean
+}
+
 export interface IGuildQueryOuptut {
   quoteSettings: IQuoteSettings
   settings: ISettings
 
-  // `doesExist` tells the query handler to return the guild default values if the guild is not found
+  // plays with `existingOnly`; will be true if the value that was returned was from actual guild record and not just defaults
   doesExist?: boolean
 }
 
-export interface IGuildQueryInput {
-  guildId: string
-  existingOnly?: boolean
-}
-
+/**
+ * Query for guild information.
+ */
 export class GuildQuery implements IQuery {
   constructor(readonly input: IGuildQueryInput) {}
 }
