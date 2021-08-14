0968c1f85369bae3654d58d26ed7af29dc8b17c9
Felix <jonfelixrico@gmail.com>
2021-08-14 22:44:48 +0800

    Generate code for new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,8 @@ import { ReadModelQueryModule } from './read-model-query/read-model-query.module
 import { EventHandlersModule } from './event-handlers/event-handlers.module'
 import { EsdbBackupModule } from './esdb-backup/esdb-backup.module'
 import { StatsModelModule } from './stats-model/stats-model.module'
+import { DiscordWatchersModule } from './discord-watchers/discord-watchers.module'
+import { InteractionHandlersModule } from './interaction-handlers/interaction-handlers.module'
 
 @Module({
   imports: [
@@ -34,6 +36,8 @@ import { StatsModelModule } from './stats-model/stats-model.module'
     EventHandlersModule,
     EsdbBackupModule,
     StatsModelModule,
+    DiscordWatchersModule,
+    InteractionHandlersModule,
   ],
 })
 export class AppModule {}
