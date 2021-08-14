44dbb231e8a707cfc6ddc2c6a77b6a70805b1b9a
Felix <jonfelixrico@gmail.com>
2021-08-15 00:30:11 +0800

    Add the ReadModelQueryModule as one of the imports of DiscordInteractionsModule

--- src/discord-interactions/discord-interactions.module.ts
+++ src/discord-interactions/discord-interactions.module.ts
@@ -4,9 +4,10 @@ import { DiscordModule } from 'src/discord/discord.module'
 import { InteractionCreatedRelayService } from './services/interaction-created-relay/interaction-created-relay.service'
 import { ReceiveInteractionHandlerService } from './handlers/receive-interaction-handler/receive-interaction-handler.service'
 import { CommandManagerService } from './services/command-manager/command-manager.service'
+import { ReadModelQueryModule } from 'src/read-model-query/read-model-query.module'
 
 @Module({
-  imports: [DiscordModule, CqrsModule],
+  imports: [DiscordModule, CqrsModule, ReadModelQueryModule],
   providers: [
     InteractionCreatedRelayService,
     ReceiveInteractionHandlerService,
