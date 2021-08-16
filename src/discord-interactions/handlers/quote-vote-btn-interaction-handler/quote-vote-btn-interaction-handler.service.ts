0d0adb4f13903afabf7af7734b24669994a5b34e
Felix <jonfelixrico@gmail.com>
2021-08-17 00:13:30 +0800

    Push partial implementation for quote voting

--- src/discord-interactions/handlers/quote-vote-btn-interaction-handler/quote-vote-btn-interaction-handler.service.ts
+++ src/discord-interactions/handlers/quote-vote-btn-interaction-handler/quote-vote-btn-interaction-handler.service.ts
@@ -1,5 +1,10 @@
-import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
+import { Logger } from '@nestjs/common'
+import { EventsHandler, IEventHandler, QueryBus } from '@nestjs/cqrs'
 import { DiscordInteractionEvent } from 'src/discord-interactions/types/discord-interaction.event'
+import {
+  IPendingQuoteQueryOutput,
+  PendingQuoteQuery,
+} from 'src/queries/pending-quote.query'
 
 const BUTTON_ID_REGEXP = /quote\/(.+)\/vote\/(-?\d+)/
 
@@ -7,6 +12,8 @@ const BUTTON_ID_REGEXP = /quote\/(.+)\/vote\/(-?\d+)/
 export class QuoteVoteBtnInteractionHandlerService
   implements IEventHandler<DiscordInteractionEvent>
 {
+  constructor(private queryBus: QueryBus, private logger: Logger) {}
+
   async handle({ interaction }: DiscordInteractionEvent) {
     if (
       !interaction.isButton() ||
@@ -15,8 +22,42 @@ export class QuoteVoteBtnInteractionHandlerService
       return
     }
 
-    const [quoteId, vote] = BUTTON_ID_REGEXP.exec(interaction.customId).slice(1)
+    const [quoteId, valueStr] = BUTTON_ID_REGEXP.exec(
+      interaction.customId,
+    ).slice(1)
+    const voteValue = parseInt(valueStr)
+
     await interaction.deferReply({ ephemeral: true })
-    await interaction.editReply(`ack: ${quoteId} ${vote}`)
+
+    const { logger } = this
+
+    const pendingQuote = await this.queryBus.execute<
+      PendingQuoteQuery,
+      IPendingQuoteQueryOutput
+    >(new PendingQuoteQuery({ quoteId }))
+
+    if (!pendingQuote) {
+      logger.warn(
+        `Attempted to vote for missing quote ${quoteId}.`,
+        QuoteVoteBtnInteractionHandlerService.name,
+      )
+      return await interaction.editReply(
+        'Looks like the data of the quote you voted for is missing...',
+      )
+    } else if (
+      pendingQuote.acceptDt ||
+      pendingQuote.expireAckDt ||
+      pendingQuote.cancelDt
+    ) {
+      logger.warn(
+        `Attempted to vote for non-pending quote ${quoteId}.`,
+        QuoteVoteBtnInteractionHandlerService.name,
+      )
+      return await interaction.editReply(
+        'The quote that you voted for no longer accepts votes.',
+      )
+    }
+
+    // TODO add continuation for the vote logic here
   }
 }
