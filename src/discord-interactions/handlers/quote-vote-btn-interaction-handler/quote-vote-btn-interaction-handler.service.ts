9578ec9b1738b037a99fee2136a644f242b48983
Felix <jonfelixrico@gmail.com>
2021-08-17 00:39:19 +0800

    Integrate the vote cast command into QuoteVoteBtnInteractionHandlerService

--- src/discord-interactions/handlers/quote-vote-btn-interaction-handler/quote-vote-btn-interaction-handler.service.ts
+++ src/discord-interactions/handlers/quote-vote-btn-interaction-handler/quote-vote-btn-interaction-handler.service.ts
@@ -1,6 +1,12 @@
 import { Logger } from '@nestjs/common'
-import { EventsHandler, IEventHandler, QueryBus } from '@nestjs/cqrs'
+import {
+  CommandBus,
+  EventsHandler,
+  IEventHandler,
+  QueryBus,
+} from '@nestjs/cqrs'
 import { DiscordInteractionEvent } from 'src/discord-interactions/types/discord-interaction.event'
+import { CastPendingQuoteVoteCommand } from 'src/domain/commands/cast-pending-quote-vote.command'
 import {
   IPendingQuoteQueryOutput,
   PendingQuoteQuery,
@@ -8,11 +14,20 @@ import {
 
 const BUTTON_ID_REGEXP = /quote\/(.+)\/vote\/(-?\d+)/
 
+const EMOJI_MAPPING = {
+  1: '👍',
+  2: '👎',
+}
+
 @EventsHandler(DiscordInteractionEvent)
 export class QuoteVoteBtnInteractionHandlerService
   implements IEventHandler<DiscordInteractionEvent>
 {
-  constructor(private queryBus: QueryBus, private logger: Logger) {}
+  constructor(
+    private queryBus: QueryBus,
+    private logger: Logger,
+    private commandBus: CommandBus,
+  ) {}
 
   async handle({ interaction }: DiscordInteractionEvent) {
     if (
@@ -22,14 +37,19 @@ export class QuoteVoteBtnInteractionHandlerService
       return
     }
 
+    const { logger } = this
     const [quoteId, valueStr] = BUTTON_ID_REGEXP.exec(
       interaction.customId,
     ).slice(1)
     const voteValue = parseInt(valueStr)
+    const userId = interaction.user.id
+
+    logger.verbose(
+      `Received vote interaction from user ${userId} for quote ${quoteId}.`,
+      QuoteVoteBtnInteractionHandlerService.name,
+    )
 
     await interaction.deferReply({ ephemeral: true })
-
-    const { logger } = this
 
     const pendingQuote = await this.queryBus.execute<
       PendingQuoteQuery,
@@ -41,9 +61,11 @@ export class QuoteVoteBtnInteractionHandlerService
         `Attempted to vote for missing quote ${quoteId}.`,
         QuoteVoteBtnInteractionHandlerService.name,
       )
-      return await interaction.editReply(
+
+      await interaction.editReply(
         'Looks like the data of the quote you voted for is missing...',
       )
+      return
     } else if (
       pendingQuote.acceptDt ||
       pendingQuote.expireAckDt ||
@@ -53,11 +75,40 @@ export class QuoteVoteBtnInteractionHandlerService
         `Attempted to vote for non-pending quote ${quoteId}.`,
         QuoteVoteBtnInteractionHandlerService.name,
       )
-      return await interaction.editReply(
+
+      await interaction.editReply(
         'The quote that you voted for no longer accepts votes.',
       )
+      return
     }
 
-    // TODO add continuation for the vote logic here
+    // TODO ask confirmation for vote change
+
+    await this.commandBus.execute(
+      new CastPendingQuoteVoteCommand({
+        quoteId,
+        userId: interaction.user.id,
+        voteValue,
+      }),
+    )
+
+    const hasVoted = pendingQuote.votes.some((vote) => vote.userId === userId)
+
+    if (!hasVoted) {
+      logger.log(
+        `User ${userId} has casted their vote for quote ${quoteId} (${voteValue})`,
+        QuoteVoteBtnInteractionHandlerService.name,
+      )
+      await interaction.editReply(`You have voted ${EMOJI_MAPPING[voteValue]}.`)
+      return
+    }
+
+    logger.log(
+      `User ${userId} has changed their vote for quote ${quoteId} (${voteValue})`,
+      QuoteVoteBtnInteractionHandlerService.name,
+    )
+    await interaction.editReply(
+      `You have changed your vote to ${EMOJI_MAPPING[voteValue]}.`,
+    )
   }
 }
