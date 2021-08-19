06c4aaa1157ec655fcbc5ff7454d03d81e59e01c
Felix <jonfelixrico@gmail.com>
2021-08-19 23:54:33 +0800

    Fix module imports

--- src/message-id-cache-model/message-id-cache-model.module.ts
+++ src/message-id-cache-model/message-id-cache-model.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
+import { EventStoreModule } from 'src/event-store/event-store.module'
 import { redisProviders } from './redis.providers'
 import { CacheCatchUpService } from './services/cache-catch-up/cache-catch-up.service'
 
 @Module({
+  imports: [EventStoreModule],
   providers: [...redisProviders, CacheCatchUpService],
 })
 export class MessageIdCacheModelModule {}
