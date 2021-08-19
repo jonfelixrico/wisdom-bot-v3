cab2297d4c9a50e9c60eaf7b6aaaaaf1a033e39f
Felix <jonfelixrico@gmail.com>
2021-08-20 00:25:19 +0800

    Create query handler for checking if a message is being watched

--- src/message-id-cache-model/message-id-cache-model.module.ts
+++ src/message-id-cache-model/message-id-cache-model.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common'
 import { EventStoreModule } from 'src/event-store/event-store.module'
 import { redisProviders } from './redis.providers'
 import { CacheCatchUpService } from './services/cache-catch-up/cache-catch-up.service'
+import { WatchedMessageQueryHandlerService } from './handlers/watched-message-query-handler/watched-message-query-handler.service'
 
 @Module({
   imports: [EventStoreModule],
-  providers: [...redisProviders, CacheCatchUpService],
+  providers: [
+    ...redisProviders,
+    CacheCatchUpService,
+    WatchedMessageQueryHandlerService,
+  ],
 })
 export class MessageIdCacheModelModule {}
