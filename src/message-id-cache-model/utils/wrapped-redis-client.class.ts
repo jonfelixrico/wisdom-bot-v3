749b78bb552af1e73e5ee0ebb1b8acbf0581133a
Felix <jonfelixrico@gmail.com>
2021-08-19 22:55:02 +0800

    Apply changes to WrappedRedisClient

--- src/message-id-cache-model/utils/wrapped-redis-client.class.ts
+++ src/message-id-cache-model/utils/wrapped-redis-client.class.ts
@@ -5,8 +5,12 @@ export class WrappedRedisClient {
   readonly get: (key: string) => Promise<string>
   readonly set: (key: string, value: string) => Promise<unknown>
 
-  constructor(readonly client: RedisClient) {
+  private constructor(readonly client: RedisClient) {
     this.get = promisify(client.get).bind(client)
     this.set = promisify(client.set).bind(client)
+  }
+
+  static wrapClient(client: RedisClient) {
+    return new WrappedRedisClient(client)
   }
 }
