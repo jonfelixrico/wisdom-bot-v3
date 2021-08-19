b5024be78b16b8535255f29b912d426eb7787357
Felix <jonfelixrico@gmail.com>
2021-08-19 23:33:28 +0800

    Create reducers for message caching

---
+++ src/message-id-cache-model/reducers.ts
@@ -0,0 +1,89 @@
+import {
+  AllStreamJSONRecordedEvent,
+  JSONEventType,
+  JSONType,
+} from '@eventstore/db-client'
+import { DomainEventNames } from 'src/domain/domain-event-names.enum'
+import { IPendingQuoteAcceptedPayload } from 'src/domain/events/pending-quote-accepted.event'
+import { IPendingQuoteCancelledPayload } from 'src/domain/events/pending-quote-cancelled.event'
+import { IPendingQuoteExpirationAcknowledgedEventPayload } from 'src/domain/events/pending-quote-expiration-acknowledged.event'
+import { IQuoteMessageDetailsUpdatedPayload } from 'src/domain/events/quote-message-details-updated.event'
+import { IQuoteSubmittedEventPayload } from 'src/domain/events/quote-submitted.event'
+import { promisify } from 'util'
+import { WrappedRedisClient } from './utils/wrapped-redis-client.class'
+
+export type CacheReducer<EventDataType extends JSONType = JSONType> = (
+  // This is directly taken from EventStoreDB
+  event: AllStreamJSONRecordedEvent<JSONEventType<string, EventDataType>>,
+  client: WrappedRedisClient,
+) => Promise<void> // true if processed, false if skipped
+
+type CacheReducerMap = {
+  [key: string]: CacheReducer
+}
+
+const POSITION = 'POSITION'
+
+const onSubmit: CacheReducer<IQuoteSubmittedEventPayload> = async (
+  { position, data },
+  { client },
+) => {
+  const { messageId, quoteId } = data
+
+  const chain = client
+    .multi()
+    .set(messageId, quoteId)
+    .set(quoteId, messageId)
+    .set(POSITION, JSON.stringify(position))
+  await promisify(chain.exec).call(chain)
+}
+
+const onUpdate: CacheReducer<IQuoteMessageDetailsUpdatedPayload> = async (
+  { position, data },
+  wrapped,
+) => {
+  const { messageId, quoteId } = data
+
+  const oldMessageId = await wrapped.get(quoteId)
+
+  const chain = wrapped.client
+    .multi()
+    .set(quoteId, messageId)
+    .set(messageId, quoteId)
+    .del(oldMessageId)
+    .set(POSITION, JSON.stringify(position))
+  await promisify(chain.exec).call(chain)
+}
+
+const onPendingEnd: CacheReducer<
+  | IPendingQuoteExpirationAcknowledgedEventPayload
+  | IPendingQuoteAcceptedPayload
+  | IPendingQuoteCancelledPayload
+> = async ({ position, data }, wrapped) => {
+  const { quoteId } = data
+
+  const messageId = await wrapped.get(quoteId)
+
+  const chain = wrapped.client
+    .multi()
+    .del(quoteId)
+    .del(messageId)
+    .set(POSITION, JSON.stringify(position))
+  await promisify(chain.exec).call(chain)
+}
+
+const {
+  QUOTE_SUBMITTED,
+  PENDING_QUOTE_EXPIRATION_ACKNOWLEDGED,
+  PENDING_QUOTE_CANCELLED,
+  PENDING_QUOTE_ACCEPTED,
+  QUOTE_MESSAGE_DETAILS_UPDATED,
+} = DomainEventNames
+
+export const CACHE_REDUCERS: CacheReducerMap = {
+  [QUOTE_SUBMITTED]: onSubmit,
+  [QUOTE_MESSAGE_DETAILS_UPDATED]: onUpdate,
+  [PENDING_QUOTE_EXPIRATION_ACKNOWLEDGED]: onPendingEnd,
+  [PENDING_QUOTE_CANCELLED]: onPendingEnd,
+  [PENDING_QUOTE_ACCEPTED]: onPendingEnd,
+}
