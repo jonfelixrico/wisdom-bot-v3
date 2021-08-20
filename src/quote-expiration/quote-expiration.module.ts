95bcaf5fb62239801bcef756726e817a1576e693
Felix <jonfelixrico@gmail.com>
2021-08-20 23:25:08 +0800

    Create placeholder for the catch up service

--- src/quote-expiration/quote-expiration.module.ts
+++ src/quote-expiration/quote-expiration.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { EventStoreModule } from 'src/event-store/event-store.module'
 import { dbProvider } from './db/db.provider'
+import { QuoteExpirationCatchUpService } from './event-sourcing/quote-expiration-catch-up/quote-expiration-catch-up.service'
 
 @Module({
   imports: [EventStoreModule],
-  providers: [dbProvider],
+  providers: [dbProvider, QuoteExpirationCatchUpService],
 })
 export class QuoteExpirationModule {}
