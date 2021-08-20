a26aaced18b7aa761bf933bd38957ad54842bf7e
Felix <jonfelixrico@gmail.com>
2021-08-20 23:19:58 +0800

    Add in the MapTypeormEntity in the DbProvider's createConnection call

--- src/quote-expiration/providers/db.provider.ts
+++ src/quote-expiration/providers/db.provider.ts
@@ -1,18 +1,18 @@
 import { Provider } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { MapTypeormEntity } from 'src/typeorm/entities/map.typeorm-entity'
 import { Connection, createConnection } from 'typeorm'
 
 export const dbProvider: Provider = {
   provide: Connection,
   inject: [ConfigService],
-  useFactory: async (cfg: ConfigService) => {
+  useFactory: async (cfg: ConfigService): Promise<Connection> =>
     await createConnection({
       type: 'postgres',
       url: cfg.get('READ_DB_URL'),
       synchronize: !!cfg.get('READ_DB_SYNC'),
-      entities: [__dirname + '/**/*.typeorm-entity{.ts,.js}'],
+      entities: [__dirname + '/**/*.typeorm-entity{.ts,.js}', MapTypeormEntity],
       name: 'expiration-model',
       schema: 'expiration',
-    })
-  },
+    }),
 }
