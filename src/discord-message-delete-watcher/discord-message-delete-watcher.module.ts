6c684023fa8b5bb3ab7afb02419177f050b3c60f
Felix <jonfelixrico@gmail.com>
2021-08-20 22:43:26 +0800

    Apply minor relocations

--- src/discord-message-delete-watcher/discord-message-delete-watcher.module.ts
+++ src/discord-message-delete-watcher/discord-message-delete-watcher.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { EventStoreModule } from 'src/event-store/event-store.module'
 import { redisProviders } from './redis.providers'
 import { WatchedMessagesCatchUp } from './services/watched-messages-catch-up/watched-messages-catch-up.service'
-import { WatchedMessageQueryHandlerService } from './handlers/watched-message-query-handler/watched-message-query-handler.service'
+import { WatchedMessageQueryHandlerService } from './query-handlers/watched-message-query-handler/watched-message-query-handler.service'
 import { DiscordModule } from 'src/discord/discord.module'
 
 @Module({
