7908c178dbb199b0abb21a349f3f25e1e6862bee
Felix <jonfelixrico@gmail.com>
2021-08-20 23:23:37 +0800

    Import the EventStoreModule

--- src/quote-expiration/quote-expiration.module.ts
+++ src/quote-expiration/quote-expiration.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { EventStoreModule } from 'src/event-store/event-store.module'
 import { dbProvider } from './db/db.provider'
 
 @Module({
+  imports: [EventStoreModule],
   providers: [dbProvider],
 })
 export class QuoteExpirationModule {}
