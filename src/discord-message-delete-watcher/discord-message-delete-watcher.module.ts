770a96f1adad125ea521cb3ad4399dfc34067208
Felix <jonfelixrico@gmail.com>
2021-08-20 22:38:58 +0800

    Import the discord module

--- src/discord-message-delete-watcher/discord-message-delete-watcher.module.ts
+++ src/discord-message-delete-watcher/discord-message-delete-watcher.module.ts
@@ -3,9 +3,10 @@ import { EventStoreModule } from 'src/event-store/event-store.module'
 import { redisProviders } from './redis.providers'
 import { CacheCatchUpService } from './services/cache-catch-up/cache-catch-up.service'
 import { WatchedMessageQueryHandlerService } from './handlers/watched-message-query-handler/watched-message-query-handler.service'
+import { DiscordModule } from 'src/discord/discord.module'
 
 @Module({
-  imports: [EventStoreModule],
+  imports: [EventStoreModule, DiscordModule],
   providers: [
     ...redisProviders,
     CacheCatchUpService,
