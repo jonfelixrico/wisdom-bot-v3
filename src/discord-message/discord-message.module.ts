67eefe1931f56d354012218489af500a5c3a92dd
Felix <jonfelixrico@gmail.com>
2021-08-21 00:57:07 +0800

    Add discord provider under DiscordMessageModule

--- src/discord-message/discord-message.module.ts
+++ src/discord-message/discord-message.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { dbProvider } from './db/db.provider'
+import { redisProviders } from './redis/redis.providers'
 
 @Module({
-  providers: [dbProvider],
+  providers: [dbProvider, ...redisProviders],
 })
 export class DiscordMessageModule {}
