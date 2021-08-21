0aaa9b9979cc0c9a4300c8529638a87bb7af9054
Felix <jonfelixrico@gmail.com>
2021-08-22 00:30:34 +0800

    Remove unused query services from the guild model

--- src/read-model-query/read-model-query.module.ts
+++ src/read-model-query/read-model-query.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common'
 import { TypeormModule } from 'src/typeorm/typeorm.module'
-import { GuildStatsQueryService } from './guild-stats-query/guild-stats-query.service'
 import { PendingQuoteQueryService } from './pending-quote-query/pending-quote-query.service'
 import { QuoteQueryService } from './quote-query/quote-query.service'
 import { ReceiveQueryService } from './receive-query/receive-query.service'
-import { UserStatsQueryService } from './user-stats-query/user-stats-query.service'
 import { GuildQueryHandlerService } from './handlers/guild-query-handler/guild-query-handler.service'
 import { PendingQuoteQueryHandlerService } from './handlers/pending-quote-query-handler/pending-quote-query-handler.service'
 
 const exportedModules = [
   QuoteQueryService,
   PendingQuoteQueryService,
-  GuildStatsQueryService,
-  UserStatsQueryService,
   ReceiveQueryService,
 ]
 
