be983f6f3cfd492460b856a7319d1e864e2b7baa
Felix <jonfelixrico@gmail.com>
2021-08-22 15:49:40 +0800

    Use GuildMemeberTypeormEntity in GuildTopReceivedAuthorsQueryHandler

--- src/stats-model/queries/handlers/guild-top-received-authors-query-handler/guild-top-received-authors-query-handler.service.ts
+++ src/stats-model/queries/handlers/guild-top-received-authors-query-handler/guild-top-received-authors-query-handler.service.ts
@@ -1,15 +1,10 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
-import { GuildMemberInteractionTypeormEntity } from 'src/stats-model/db/entities/guild-member-interaction.typeorm-entity'
-import { Connection } from 'typeorm'
+import { GuildMemberTypeormEntity } from 'src/stats-model/db/entities/guild-member.typeorm-entity'
+import { Connection, MoreThan } from 'typeorm'
 import {
   GuildTopReceivedAuthorsQuery,
   IGuildTopReceivedAuthorsQueryOutput,
 } from '../../guild-top-received-authors.query'
-
-interface IResult {
-  authorId: string
-  receives: number
-}
 
 @QueryHandler(GuildTopReceivedAuthorsQuery)
 export class GuildTopReceivedAuthorsQueryHandlerService
@@ -22,16 +17,24 @@ export class GuildTopReceivedAuthorsQueryHandlerService
   }: GuildTopReceivedAuthorsQuery): Promise<IGuildTopReceivedAuthorsQueryOutput> {
     const { guildId, limit } = input
 
-    return await this.conn
-      .getRepository(GuildMemberInteractionTypeormEntity)
-      .createQueryBuilder()
-      .select('"targetUserId"', 'authorId')
-      .addSelect('SUM(receives)', 'receives')
-      .where('"guildId" = :guildId', { guildId })
-      .groupBy('"targetUserId"')
-      .having('SUM(receives) > 0')
-      .orderBy('receives', 'DESC')
-      .limit(limit)
-      .getRawMany<IResult>()
+    const topReceived = await this.conn
+      .getRepository(GuildMemberTypeormEntity)
+      .find({
+        where: {
+          guildId,
+          quoteReceives: MoreThan(0),
+        },
+        take: limit,
+        order: {
+          quoteReceives: 'DESC',
+        },
+      })
+
+    return topReceived.map(({ userId, quoteReceives }) => {
+      return {
+        authorId: userId,
+        receives: quoteReceives,
+      }
+    })
   }
 }
