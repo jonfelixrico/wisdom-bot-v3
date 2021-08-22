0800662608f7fd3d6b4a4dcfc8ec287eab6337bf
Felix <jonfelixrico@gmail.com>
2021-08-22 15:53:09 +0800

    Make GuildTopContributorsQueryHandler use GuildMemberTypeormEntity

--- src/stats-model/queries/handlers/guild-top-contributors-query-handler/guild-top-contributors-query-handler.service.ts
+++ src/stats-model/queries/handlers/guild-top-contributors-query-handler/guild-top-contributors-query-handler.service.ts
@@ -1,15 +1,10 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
-import { GuildMemberInteractionTypeormEntity } from 'src/stats-model/db/entities/guild-member-interaction.typeorm-entity'
-import { Connection } from 'typeorm'
+import { GuildMemberTypeormEntity } from 'src/stats-model/db/entities/guild-member.typeorm-entity'
+import { Connection, MoreThan } from 'typeorm'
 import {
   GuildTopContributorsQuery,
   IGuildTopContributorsQueryOutput,
 } from '../../guild-top-contributors.query'
-
-interface IResult {
-  userId: string
-  contributions: number
-}
 
 @QueryHandler(GuildTopContributorsQuery)
 export class GuildTopContributorsQueryHandlerService
@@ -17,26 +12,29 @@ export class GuildTopContributorsQueryHandlerService
 {
   constructor(private conn: Connection) {}
 
-  private get repo() {
-    return this.conn.getRepository(GuildMemberInteractionTypeormEntity)
-  }
-
   async execute({
     input,
   }: GuildTopContributorsQuery): Promise<IGuildTopContributorsQueryOutput> {
     const { guildId, limit } = input
 
-    const results = await this.repo
-      .createQueryBuilder()
-      .select('"userId"')
-      .addSelect('SUM("submitted")', 'contributions')
-      .where('"guildId" = :guildId', { guildId })
-      .groupBy('"userId"')
-      .having('SUM("submitted") > 0')
-      .orderBy('contributions', 'DESC')
-      .limit(limit)
-      .getRawMany<IResult>()
+    const contributors = await this.conn
+      .getRepository(GuildMemberTypeormEntity)
+      .find({
+        where: {
+          guildId,
+          submissions: MoreThan(0),
+        },
+        take: limit,
+        order: {
+          submissions: 'DESC',
+        },
+      })
 
-    return results
+    return contributors.map(({ userId, submissions }) => {
+      return {
+        userId,
+        contributions: submissions,
+      }
+    })
   }
 }
