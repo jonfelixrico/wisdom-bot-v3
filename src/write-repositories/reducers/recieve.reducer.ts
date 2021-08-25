206300e385ed600ad404610fe3dff2b84e26fbbc
Felix <jonfelixrico@gmail.com>
2021-08-25 20:37:34 +0800

    Integrate RECEIVE_REACTION_WITHDRAWN into the write reducers for the receive entity

--- src/write-repositories/reducers/recieve.reducer.ts
+++ src/write-repositories/reducers/recieve.reducer.ts
@@ -2,6 +2,7 @@ import { DomainEventNames } from 'src/domain/domain-event-names.enum'
 import { IReceiveEntity } from 'src/domain/entities/receive.entity'
 import { IReceiveCreatedPayload } from 'src/domain/events/receive-created.event'
 import { IReceiveReactedPayload } from 'src/domain/events/receive-reacted.event'
+import { IReceiveReactionWithdrawnEventPayload } from 'src/domain/events/receive-reaction-withdrawn.event'
 import {
   WriteRepositoryReducer,
   WriteRepositoryReducerMap,
@@ -17,17 +18,42 @@ const create: WriteRepositoryReducer<IReceiveCreatedPayload, IReceiveEntity> = (
 }
 
 const react: WriteRepositoryReducer<IReceiveReactedPayload, IReceiveEntity> = (
-  { receiveId, reactionId, userId, karma },
+  { receiveId, userId, karma },
   { reactions, ...state },
 ) => {
   return {
     ...state,
-    reactions: [...reactions, { receiveId, reactionId, userId, karma }],
+    reactions: [...reactions, { receiveId, userId, karma }],
   }
 }
 
-const { RECEIVE_CREATED, RECEIVE_REACTED } = DomainEventNames
+const reactionWithdrawn: WriteRepositoryReducer<
+  IReceiveReactionWithdrawnEventPayload,
+  IReceiveEntity
+> = ({ userId }, { reactions, ...state }) => {
+  const index = reactions.findIndex((reaction) => reaction.userId === userId)
+
+  if (index === -1) {
+    return {
+      ...state,
+      reactions,
+    }
+  }
+
+  // Good practice to not mutate the original array; Array#splice is a mutating method
+  const clone = [...reactions]
+  clone.splice(index, 1)
+
+  return {
+    ...state,
+    reactions: clone,
+  }
+}
+
+const { RECEIVE_CREATED, RECEIVE_REACTED, RECEIVE_REACTION_WITHDRAWN } =
+  DomainEventNames
 export const RECEIVE_REDUCERS: WriteRepositoryReducerMap<IReceiveEntity> = {
   [RECEIVE_CREATED]: create,
   [RECEIVE_REACTED]: react,
+  [RECEIVE_REACTION_WITHDRAWN]: reactionWithdrawn,
 }
