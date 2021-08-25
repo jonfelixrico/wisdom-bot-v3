5ce52703d5f51d49a8feab72bc11d3be710d1b89
Felix <jonfelixrico@gmail.com>
2021-08-25 21:49:24 +0800

    Remove the ReceiveQueryService

--- src/read-model-query/read-model-query.module.ts
+++ src/read-model-query/read-model-query.module.ts
@@ -2,16 +2,11 @@ import { Module } from '@nestjs/common'
 import { TypeormModule } from 'src/typeorm/typeorm.module'
 import { PendingQuoteQueryService } from './pending-quote-query/pending-quote-query.service'
 import { QuoteQueryService } from './quote-query/quote-query.service'
-import { ReceiveQueryService } from './receive-query/receive-query.service'
 import { GuildQueryHandlerService } from './handlers/guild-query-handler/guild-query-handler.service'
 import { PendingQuoteQueryHandlerService } from './handlers/pending-quote-query-handler/pending-quote-query-handler.service'
 import { ReceiveQueryHandlerService } from './handlers/receive-query-handler/receive-query-handler.service'
 
-const exportedModules = [
-  QuoteQueryService,
-  PendingQuoteQueryService,
-  ReceiveQueryService,
-]
+const exportedModules = [QuoteQueryService, PendingQuoteQueryService]
 
 @Module({
   imports: [TypeormModule],
