645b662202baec976ee5370c31d98a8695c1aff0
Felix <jonfelixrico@gmail.com>
2021-08-26 00:56:54 +0800

    Apply corrections to events and commands

--- src/domain/commands/receive-quote.command.ts
+++ src/domain/commands/receive-quote.command.ts
@@ -1,19 +1,12 @@
 import { ICommand } from '../command.interface'
 
-interface IBasePayload {
+export interface IReceiveQuoteCommandPayload {
   readonly quoteId: string
   readonly channelId: string
   readonly userId: string
+  readonly messageId?: string
+  readonly interactionToken?: string
 }
-
-interface IMessagePayload extends IBasePayload {
-  messageId: string
-}
-
-interface IInteractionPayload extends IBasePayload {
-  interactionToken: string
-}
-export type IReceiveQuoteCommandPayload = IMessagePayload | IInteractionPayload
 
 export class ReceiveQuoteCommand
   implements ICommand<IReceiveQuoteCommandPayload>
--- src/domain/events/receive-created.event.ts
+++ src/domain/events/receive-created.event.ts
@@ -2,25 +2,16 @@ import { DomainEvent } from '../abstracts/domain-event.abstract'
 import { DomainEventNames } from '../domain-event-names.enum'
 import { EventPayload } from './event-payload.type'
 
-interface IBasePayload extends EventPayload {
+export interface IReceiveCreatedPayload extends EventPayload {
   readonly receiveId: string
   readonly quoteId: string
   readonly userId: string
   readonly guildId: string
   readonly channelId: string
   readonly receiveDt: Date
+  readonly messageId?: string
+  readonly interactionToken?: string
 }
-
-interface IMessagePayload extends IBasePayload {
-  messageId: string
-}
-
-interface IInteractionPayload extends IBasePayload {
-  interactionToken: string
-}
-
-export type IReceiveCreatedPayload = IMessagePayload | IInteractionPayload
-
 export class ReceiveCreatedEvent extends DomainEvent<IReceiveCreatedPayload> {
   constructor(receive: IReceiveCreatedPayload) {
     super(DomainEventNames.RECEIVE_CREATED, receive.receiveId, receive)
