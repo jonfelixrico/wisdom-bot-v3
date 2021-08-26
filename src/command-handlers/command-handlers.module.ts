b74ddb1a1b53db89feab9b37ba08a5ecb1e8c3cb
Felix <jonfelixrico@gmail.com>
2021-08-26 22:03:28 +0800

    Push changes caused by running the `format` script

--- src/command-handlers/command-handlers.module.ts
+++ src/command-handlers/command-handlers.module.ts
@@ -8,7 +8,7 @@ import { UpdateQuoteMessageDetailsCommandHandlerService } from './pending-quote/
 import { DiscordModule } from 'src/discord/discord.module'
 import { AcknowledgeQuoteExpirationCommandHandlerService } from './pending-quote/acknowledge-quote-expiration-command-handler/acknowledge-quote-expiration-command-handler.service'
 import { CastPendingQuoteVoteCommandHandlerService } from './pending-quote/cast-pending-quote-vote-command-handler/cast-pending-quote-vote-command-handler.service'
-import { UpdateReceiveMessageDetailsCommandHandlerService } from './receive/update-receive-message-details-command-handler/update-receive-message-details-command-handler.service';
+import { UpdateReceiveMessageDetailsCommandHandlerService } from './receive/update-receive-message-details-command-handler/update-receive-message-details-command-handler.service'
 
 @Module({
   imports: [WriteRepositoriesModule, DiscordModule],
